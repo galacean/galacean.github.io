16eecb698200c9c7323af47dd060af62e4decea2
eyworldwide <eyworldwide@foxmail.com>
2021-05-03 01:15:17 +0800

    make typedoc better

--- src/components/typedoc/Comment.tsx
+++ src/components/typedoc/Comment.tsx
@@ -1,17 +1,15 @@
-import { SmileTwoTone } from '@ant-design/icons';
 import React from 'react';
 import { IComment, ITag } from './interface';
 
 function Tag (props: ITag) {
   return <p>
-    <span className="tsd-comment-tags">{props.tag}</span> {props.text}
+    <span className={`tsd-comment-tag tsd-comment-tag-${props.tag}`}>{props.tag}</span>{props.text}
   </p>
 }
 export default function Comment(props: IComment) {
   return <blockquote className="tsd-comment">
-    {props.shortText && <p><SmileTwoTone /> {props.shortText}</p>}
+    {props.shortText && <p>{props.shortText}</p>}
     {props.tags && props.tags.map((tag, i) => {
-      console.log('tag', tag)
       return <Tag key={i} {...tag}/>
     })}
   </blockquote>;
