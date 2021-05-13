10674b009b1b39eefdcf59307f9e08bb3fa5e41d
eyworldwide <eyworldwide@foxmail.com>
2021-05-13 21:03:02 +0800

    add CNAME

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,11 +5,11 @@ exports.createPages = require('./gatsby/createPages');
 exports.onCreateNode = require('./gatsby/onCreateNode');
 exports.onCreatePage = require('./gatsby/onCreatePage');
 
-// exports.onPostBuild = () => {
-//   // 复制 CNAME
-//   fsExtra.copyFileSync(`${__dirname}/CNAME`, `${__dirname}/public/CNAME`, (err) => {
-//     if (err) {
-//       console.error('Error copying file', err);
-//     }
-//   });
-// };
+exports.onPostBuild = () => {
+  // 复制 CNAME
+  fsExtra.copyFileSync(`${__dirname}/CNAME`, `${__dirname}/public/CNAME`, (err) => {
+    if (err) {
+      console.error('Error copying file', err);
+    }
+  });
+};
