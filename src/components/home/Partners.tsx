c3d3b38a4bcdf714cdc1b3d20eefdbc2725c4e11
eyworldwide <eyworldwide@foxmail.com>
2021-05-13 14:03:32 +0800

    add partners

--- src/components/home/Partners.tsx
+++ src/components/home/Partners.tsx
@@ -9,39 +9,53 @@ export default function Partners (props) {
       </h2>
       <div className="partners">
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*cQdHQYgR6LcAAAAAAAAAAAAAARQnAQ" width="120" alt="支付宝"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*cQdHQYgR6LcAAAAAAAAAAAAAARQnAQ" width="100"/>
         </div>
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*N1xwQ48lSOAAAAAAAAAAAAAAARQnAQ" width="120" alt="蚂蚁花呗"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*N1xwQ48lSOAAAAAAAAAAAAAAARQnAQ" width="100"/>
         </div>
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*mHrjQYHiaCMAAAAAAAAAAAAAARQnAQ" width="110" alt="借呗"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*mHrjQYHiaCMAAAAAAAAAAAAAARQnAQ" width="90"/>
         </div>
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*5gDeQp42ZI8AAAAAAAAAAAAAARQnAQ" width="170" alt="蚂蚁财富"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*auuzSpvpRCIAAAAAAAAAAAAAARQnAQ" width="130"/>
         </div>
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*0xfCSrjH0D8AAAAAAAAAAAAAARQnAQ" width="110" alt="飞猪"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*jNFlSaUPVnkAAAAAAAAAAAAAARQnAQ" width="140"/>
         </div>
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*jpHSQ7R81S8AAAAAAAAAAAAAARQnAQ" width="110" alt="alibaba.com"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*UThWSrJLu2AAAAAAAAAAAAAAARQnAQ" width="120"/>
         </div>
       </div>
       <div className="partners">
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*jNFlSaUPVnkAAAAAAAAAAAAAARQnAQ" width="140" alt="1688.com"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*5gDeQp42ZI8AAAAAAAAAAAAAARQnAQ" width="170"/>
         </div>
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*UThWSrJLu2AAAAAAAAAAAAAAARQnAQ" width="130" alt="antv"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*0xfCSrjH0D8AAAAAAAAAAAAAARQnAQ" width="130"/>
         </div>
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*A0MaTbw5gXwAAAAAAAAAAAAAARQnAQ" width="130" alt="antv"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*jpHSQ7R81S8AAAAAAAAAAAAAARQnAQ" width="90"/>
         </div>
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*MoQzQKsBd-kAAAAAAAAAAAAAARQnAQ" width="170" alt="antv"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*A0MaTbw5gXwAAAAAAAAAAAAAARQnAQ" width="100"/>
         </div>
         <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*SlVpSLS239IAAAAAAAAAAAAAARQnAQ" width="120" alt="antv"/>
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*6mvLTasKph4AAAAAAAAAAAAAARQnAQ" width="100"/>
+        </div>
+      </div>
+      <div className="partners">
+        <div className="partner">
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*KR0dSII_IwUAAAAAAAAAAAAAARQnAQ" width="100"/>
+        </div>
+        <div className="partner">
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*MoQzQKsBd-kAAAAAAAAAAAAAARQnAQ" width="150"/>
+        </div>
+        <div className="partner">
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*c-BbTpN-564AAAAAAAAAAAAAARQnAQ" width="130"/>
+        </div>
+        <div className="partner">
+          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*SlVpSLS239IAAAAAAAAAAAAAARQnAQ" width="90"/>
         </div>
       </div>
     </section>
