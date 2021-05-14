5cd7058562571758224a1ca313ee93958988a521
eyworldwide <eyworldwide@foxmail.com>
2021-05-14 16:35:06 +0800

    refactor: examples in mobiles

--- src/pages/examples.tsx
+++ src/pages/examples.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { graphql } from "gatsby";
 import WrapperLayout from '../components/layout';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Popover } from 'antd';
 import { version } from '../../siteconfig.json';
+import { MenuUnfoldOutlined } from '@ant-design/icons';
+import Media from 'react-media';
+import './examples.less';
 
 const { Sider, Content } = Layout;
 
@@ -10,25 +13,46 @@ export default function Examples(props: any) {
   const { nodes } = props.data.allPlayground;
 
   const [url, setUrl] = useState('pbr-helmet');
+  const [menuVisible, toggleMenu] = useState(false);
+
+  const menu = <Menu onSelect={(item) => { 
+    setUrl(item.key); 
+    toggleMenu(false);
+  }} style={{ width: '300px!important', height: 'calc(100vh - 102px)', overflow: 'auto'}}>
+    {nodes.map((node: any) => {
+      const { name } = node;
+      return <Menu.Item key={name}>
+        {node.name}
+      </Menu.Item>
+    })}
+  </Menu>
+
 
   return (
     <>
       <WrapperLayout {...props}>
-        <Layout hasSider={true}>
-          <Sider>
-            <Menu onSelect={(item) => { setUrl(item.key); }} style={{ width: '300px!important', height: 'calc(100vh - 102px)', overflow: 'auto'}}>
-              {nodes.map((node: any) => {
-                const { name } = node;
-                return <Menu.Item key={name}>
-                  {node.name}
-                </Menu.Item>
-              })}
-            </Menu>
-          </Sider>
-          <Content style={{height: 'calc(100vh - 102px)'}}>
-            <iframe src={`/${version}/playground/${url}`} frameBorder="0" width="100%" height="100%"></iframe>
-          </Content>
-        </Layout>
+        <Media query="(max-width: 599px)">
+          {(isMobile) => 
+            <Layout hasSider={!isMobile}>
+              {!isMobile && <Sider>{menu}</Sider>}
+              <Content style={{height: 'calc(100vh - 102px)'}}>
+                {isMobile &&
+                  <Popover
+                    className="examples-popover-menu"
+                    placement="bottomRight"
+                    content={menu}
+                    trigger="click"
+                    visible={menuVisible}
+                    arrowPointAtCenter
+                  >
+                    <MenuUnfoldOutlined className="nav-phone-icon" onClick={() => {toggleMenu(!menuVisible)}} />
+                  </Popover>
+                }
+                <iframe src={`/${version}/playground/${url}`} frameBorder="0" width="100%" height="100%"></iframe>
+              </Content>
+            </Layout>
+          }
+        </Media>
       </WrapperLayout>
     </>
   );
