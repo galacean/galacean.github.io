df63737d7035504dae4c73ca069d1b3ec63f984f
eyworldwide <eyworldwide@foxmail.com>
2021-05-14 14:58:52 +0800

    refactor: homepage responsible

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -12,7 +12,7 @@ function Banner(props) {
   return (
     <section className="home-section home-section-banner">
       <div className='home-flex'>
-        <QueueAnim className="banner-title-wrapper" type={isMobile ? 'bottom' : 'right'}>
+        <QueueAnim type={isMobile ? 'bottom' : 'right'}>
           <h1><img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*Xwt7RZ-2FrUAAAAAAAAAAAAAARQnAQ" alt="Oasis Engine" /></h1>
           <p>
             <FormattedMessage id="app.home.slogan" />
--- src/components/home/Partners.tsx
+++ src/components/home/Partners.tsx
@@ -2,62 +2,79 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 export default function Partners () {
+  const partners = [
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*cQdHQYgR6LcAAAAAAAAAAAAAARQnAQ',
+      width: 70
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*N1xwQ48lSOAAAAAAAAAAAAAAARQnAQ',
+      width: 70
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*mHrjQYHiaCMAAAAAAAAAAAAAARQnAQ',
+      width: 70
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*auuzSpvpRCIAAAAAAAAAAAAAARQnAQ',
+      width: 100
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*jNFlSaUPVnkAAAAAAAAAAAAAARQnAQ',
+      width: 100
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*UThWSrJLu2AAAAAAAAAAAAAAARQnAQ',
+      width: 90
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*5gDeQp42ZI8AAAAAAAAAAAAAARQnAQ',
+      width: 120
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*0xfCSrjH0D8AAAAAAAAAAAAAARQnAQ',
+      width: 100
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*jpHSQ7R81S8AAAAAAAAAAAAAARQnAQ',
+      width: 60
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*A0MaTbw5gXwAAAAAAAAAAAAAARQnAQ',
+      width: 70
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*6mvLTasKph4AAAAAAAAAAAAAARQnAQ',
+      width: 70
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*KR0dSII_IwUAAAAAAAAAAAAAARQnAQ',
+      width: 80
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*MoQzQKsBd-kAAAAAAAAAAAAAARQnAQ',
+      width: 100
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*c-BbTpN-564AAAAAAAAAAAAAARQnAQ',
+      width: 100
+    },
+    {
+      logo: 'https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*SlVpSLS239IAAAAAAAAAAAAAARQnAQ',
+      width: 70
+    },
+  ];
+
   return (
     <section className="home-section home-section-partners">
       <h2>
         <FormattedMessage id="app.home.partners" />
       </h2>
-      <div className="partners">
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*cQdHQYgR6LcAAAAAAAAAAAAAARQnAQ" width="100"/>
+      {partners.map((partner)=> {
+        return <div className="partner">
+          <img src={partner.logo} width={partner.width} />
         </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*N1xwQ48lSOAAAAAAAAAAAAAAARQnAQ" width="100"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*mHrjQYHiaCMAAAAAAAAAAAAAARQnAQ" width="90"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*auuzSpvpRCIAAAAAAAAAAAAAARQnAQ" width="130"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*jNFlSaUPVnkAAAAAAAAAAAAAARQnAQ" width="140"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*UThWSrJLu2AAAAAAAAAAAAAAARQnAQ" width="120"/>
-        </div>
-      </div>
-      <div className="partners">
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*5gDeQp42ZI8AAAAAAAAAAAAAARQnAQ" width="170"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*0xfCSrjH0D8AAAAAAAAAAAAAARQnAQ" width="130"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*jpHSQ7R81S8AAAAAAAAAAAAAARQnAQ" width="90"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*A0MaTbw5gXwAAAAAAAAAAAAAARQnAQ" width="100"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*6mvLTasKph4AAAAAAAAAAAAAARQnAQ" width="100"/>
-        </div>
-      </div>
-      <div className="partners">
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*KR0dSII_IwUAAAAAAAAAAAAAARQnAQ" width="100"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*MoQzQKsBd-kAAAAAAAAAAAAAARQnAQ" width="150"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*c-BbTpN-564AAAAAAAAAAAAAARQnAQ" width="130"/>
-        </div>
-        <div className="partner">
-          <img src="https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*SlVpSLS239IAAAAAAAAAAAAAARQnAQ" width="90"/>
-        </div>
-      </div>
+      })}
     </section>
   );
 }
