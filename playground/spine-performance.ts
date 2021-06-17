a46d37edf4d4fc84737c67da2715cda2ae0c888b
eyworldwide <eyworldwide@foxmail.com>
2021-06-17 17:36:13 +0800

    update spine performance

--- playground/spine-performance.ts
+++ playground/spine-performance.ts
@@ -19,7 +19,11 @@ camera.farClipPlane = 200;
 
 engine.resourceManager
   .load({
-    url: "https://sbfkcel.github.io/pixi-spine-debug/assets/spine/spineboy-pro.json",
+    urls: [ 
+      "https://gw.alipayobjects.com/os/OasisHub/a66ef194-6bc8-4325-9a59-6ea9097225b1/1620888427489.json",
+      "https://gw.alipayobjects.com/os/OasisHub/a1e3e67b-a783-4832-ba1b-37a95bd55291/1620888427490.atlas",
+      "https://gw.alipayobjects.com/zos/OasisHub/a3ca8f62-1068-43a5-bb64-5c9a0f823dde/1620888427490.png"
+    ],
     type: "spine"
   })
   .then((spineEntity: Entity) => {
