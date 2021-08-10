a2aec62edb865dda2e918b121d90be95832a8385
zhuxudong <callzhuxudong@163.com>
2021-08-10 13:40:39 +0800

    feat: add anisoLevel playground

---
+++ playground/texture-aniso.ts
@@ -0,0 +1,56 @@
+/**
+ * @title Anisotropic
+ * @category Texture
+ */
+import { OrbitControl } from "@oasis-engine/controls";
+import * as dat from "dat.gui";
+import {
+  AssetType,
+  Camera,
+  MeshRenderer,
+  PrimitiveMesh,
+  RenderFace,
+  Texture2D,
+  UnlitMaterial,
+  WebGLEngine
+} from "oasis-engine";
+const gui = new dat.GUI();
+
+// Create engine object
+const engine = new WebGLEngine("canvas");
+engine.canvas.resizeByClientSize();
+
+const scene = engine.sceneManager.activeScene;
+const rootEntity = scene.createRootEntity();
+
+// Create camera
+const cameraEntity = rootEntity.createChild("Camera");
+cameraEntity.transform.setPosition(0, 0, 1);
+cameraEntity.addComponent(Camera);
+cameraEntity.addComponent(OrbitControl);
+
+// Create Plane
+const mesh = PrimitiveMesh.createPlane(engine, 2, 2);
+const material = new UnlitMaterial(engine);
+material.renderFace = RenderFace.Double;
+const planeEntity = rootEntity.createChild("ground");
+planeEntity.transform.setRotation(-85, 0, 0);
+const planeRenderer = planeEntity.addComponent(MeshRenderer);
+planeRenderer.mesh = mesh;
+planeRenderer.setMaterial(material);
+
+engine.resourceManager
+  .load<Texture2D>({
+    url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*aHXyTqXu1_IAAAAAAAAAAAAAARQnAQ",
+    type: AssetType.Texture2D
+  })
+  .then((texture) => {
+    material.baseTexture = texture;
+    addGUI(texture);
+    engine.run();
+  });
+
+function addGUI(texture: Texture2D) {
+  const maxAnisoLevel = engine._hardwareRenderer.capability.maxAnisoLevel;
+  gui.add(texture, "anisoLevel", 1, maxAnisoLevel, 1);
+}
