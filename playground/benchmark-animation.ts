a65aab43f4ac9748f1769b4804796d199fc200a5
GuoLei1990 <gl3336563@163.com>
2021-08-26 17:56:31 +0800

    refactor: opt code

--- playground/benchmark-animation.ts
+++ playground/benchmark-animation.ts
@@ -7,10 +7,11 @@ import { Stats } from "@oasis-engine/stats";
 import { OrbitControl } from "@oasis-engine/controls";
 import { Animator, AssetType, Camera, Engine, GLTFResource, PBRMaterial, Texture2D, WebGLEngine } from "oasis-engine";
 
+Engine.registerFeature(Stats);
+
 // Create engine object.
 const engine = new WebGLEngine("canvas");
 engine.canvas.resizeByClientSize();
-Engine.registerFeature(Stats);
 
 // Create root entity and get scene.
 const scene = engine.sceneManager.activeScene;
@@ -23,7 +24,7 @@ cameraEntity.transform.setPosition(0, 0, 12);
 cameraEntity.addComponent(Camera);
 cameraEntity.addComponent(OrbitControl);
 
-// Load resources and add modles.
+// Load resources and add models.
 engine.resourceManager
   .load([
     {
@@ -35,27 +36,27 @@ engine.resourceManager
       type: AssetType.Texture2D
     }
   ])
-  .then((reources: Object[]) => {
-    const gltf = <GLTFResource>reources[0];
-    const baseTexture = <Texture2D>reources[1];
-    const huabei = gltf.defaultSceneRoot;
+  .then((resources: Object[]) => {
+    const glTF = <GLTFResource>resources[0];
+    const baseTexture = <Texture2D>resources[1];
+    const model = glTF.defaultSceneRoot;
 
-    gltf.materials.forEach((material: PBRMaterial) => {
+    glTF.materials.forEach((material: PBRMaterial) => {
       material.baseTexture = baseTexture;
       material.baseColor.setValue(1, 1, 1, 1);
     });
 
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 8; j++) {
-        const huabeiClone = huabei.clone();
-        rootEntity.addChild(huabeiClone);
+        const modelClone = model.clone();
+        rootEntity.addChild(modelClone);
 
-        const { transform } = huabeiClone;
+        const { transform } = modelClone;
         transform.setRotation(0, -90, 0);
         transform.setScale(0.5, 0.5, 0.5);
         transform.setPosition(i * 1.0 - 3.0, j * 1.2, -j * 3.5);
 
-        huabeiClone.getComponent(Animator).play("A");
+        modelClone.getComponent(Animator).play("A");
       }
     }
   });
