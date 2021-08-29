e148e0237d822a0504d20670a5434a41d8621c12
eyworldwide <eyworldwide@foxmail.com>
2021-08-29 22:10:37 +0800

    playground: update lottie demos

--- playground/lottie-benchmark.ts
+++ playground/lottie-benchmark.ts
@@ -1,10 +1,11 @@
 /**
- * @title Lottie Animation
- * @category 2D
+ * @title Lottie Benchmark
+ * @category Benchmark
  */
 import { OrbitControl } from "@oasis-engine/controls";
 import { Camera, Entity, WebGLEngine } from "oasis-engine";
 import { LottieAnimation } from "@oasis-engine/lottie";
+import '@oasis-engine/stats';
 
 const engine = new WebGLEngine("canvas");
 
@@ -14,7 +15,7 @@ const root = engine.sceneManager.activeScene.createRootEntity();
 
 const cameraEntity = root.createChild("camera");
 const camera = cameraEntity.addComponent(Camera);
-cameraEntity.transform.setPosition(0, 0, 10);
+cameraEntity.transform.setPosition(0, 0, 30);
 cameraEntity.addComponent(OrbitControl);
 
 engine.resourceManager.load<Entity>({
@@ -24,10 +25,17 @@ engine.resourceManager.load<Entity>({
   ],
   type: 'lottie'
 }).then((lottieEntity) => {
-  root.addChild(lottieEntity);
-  const lottie:LottieAnimation = lottieEntity.getComponent(LottieAnimation);
-  lottie.isLooping = true;
-  lottie.play();
+  for (let i = -4; i < 5; i++) {
+    for (let j = -5; j < 6; j++) {
+      const clone = lottieEntity.clone();
+      clone.transform.setPosition(i * 2, j * 2, 0);
+      root.addChild(clone);
+      const lottie:LottieAnimation = clone.getComponent(LottieAnimation);
+      lottie.isLooping = true;
+      lottie.speed = 0.2 + Math.random() * 2;
+      lottie.play();
+    }
+  }
 });
 
 engine.run();
