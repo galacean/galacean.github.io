7004339e8b9cbf4eac955c41e7df646d79de69a3
yangfengzzz <yangfengzzz@hotmail.com>
2021-09-08 14:39:20 +0800

    rewrite collision detection

--- playground/collision-detection.ts
+++ playground/collision-detection.ts
@@ -2,93 +2,107 @@
  * @title Collision Detection
  * @category Physics
  */
-/**
- * 本示例展示如何使用几何体渲染器功能、如何创建几何体资源对象、如何创建材质对象
- */
+
 import {
   WebGLEngine, SphereCollider,
   BoxCollider, Vector3,
   MeshRenderer, BlinnPhongMaterial, PointLight,
-  PrimitiveMesh, Camera, CollisionDetection, Script
+  PrimitiveMesh, Camera, Script
 } from "oasis-engine";
-import {OrbitControl} from "@oasis-engine/controls";
+import { OrbitControl } from "@oasis-engine/controls";
 
-const engine = new WebGLEngine("canvas");
-engine.canvas.resizeByClientSize();
-const scene = engine.sceneManager.activeScene;
-const rootEntity = scene.createRootEntity("root");
+import {
+  PhysicsEngine
+} from "../../engine/packages/physics-physx/src/index";
 
-scene.ambientLight.diffuseSolidColor.setValue(1, 1, 1, 1);
-scene.ambientLight.diffuseIntensity = 1.2;
+class Oasis {
+  static init(type: () => Promise<void>): Promise<void> {
+    return new Promise((resolve) => {
+      type().then(() => {
+        resolve();
+      });
+    });
+  }
+}
 
-// init camera
-const cameraEntity = rootEntity.createChild("camera");
-cameraEntity.addComponent(Camera);
-cameraEntity.transform.setPosition(10, 10, 10);
-cameraEntity.addComponent(OrbitControl);
+Oasis.init(PhysicsEngine.init).then(() => {
+  const engine = new WebGLEngine("canvas", new PhysicsEngine());
+  engine.canvas.resizeByClientSize();
+  const scene = engine.sceneManager.activeScene;
+  const rootEntity = scene.createRootEntity("root");
 
-// init point light
-let light = rootEntity.createChild("light");
-light.transform.setPosition(0, 3, 0);
-const pointLight = light.addComponent(PointLight);
-pointLight.intensity = 0.3;
+  scene.ambientLight.diffuseSolidColor.setValue(1, 1, 1, 1);
+  scene.ambientLight.diffuseIntensity = 1.2;
 
-// create box test entity
-const cubeSize = 2.0;
-const boxEntity = rootEntity.createChild("BoxEntity");
+  // init camera
+  const cameraEntity = rootEntity.createChild("camera");
+  cameraEntity.addComponent(Camera);
+  cameraEntity.transform.setPosition(10, 10, 10);
+  cameraEntity.addComponent(OrbitControl);
 
-const boxMtl = new BlinnPhongMaterial(engine);
-const boxRenderer = boxEntity.addComponent(MeshRenderer);
-boxMtl.baseColor.setValue(0.6, 0.3, 0.3, 1.0);
-boxRenderer.mesh = PrimitiveMesh.createCuboid(engine, cubeSize, cubeSize, cubeSize);
-boxRenderer.setMaterial(boxMtl);
+  // init point light
+  const light = rootEntity.createChild("light");
+  light.transform.setPosition(0, 3, 0);
+  const pointLight = light.addComponent(PointLight);
+  pointLight.intensity = 0.3;
 
-const boxCollider = boxEntity.addComponent(BoxCollider);
-boxCollider.setBoxCenterSize(new Vector3(), new Vector3(cubeSize, cubeSize, cubeSize));
+  // create box test entity
+  const cubeSize = 2.0;
+  const boxEntity = rootEntity.createChild("BoxEntity");
 
-// create sphere test entity
-const radius = 1.25;
-const sphereEntity = rootEntity.createChild("SphereEntity");
-sphereEntity.transform.setPosition(-2, 0, 0);
+  const boxMtl = new BlinnPhongMaterial(engine);
+  const boxRenderer = boxEntity.addComponent(MeshRenderer);
+  boxMtl.baseColor.setValue(0.6, 0.3, 0.3, 1.0);
+  boxRenderer.mesh = PrimitiveMesh.createCuboid(engine, cubeSize, cubeSize, cubeSize);
+  boxRenderer.setMaterial(boxMtl);
 
-const sphereMtl = new BlinnPhongMaterial(engine);
-const sphereRenderer = sphereEntity.addComponent(MeshRenderer);
-sphereMtl.baseColor.setValue(Math.random(), Math.random(), Math.random(), 1.0);
-sphereRenderer.mesh = PrimitiveMesh.createSphere(engine, radius);
-sphereRenderer.setMaterial(sphereMtl);
+  const boxCollider = boxEntity.addComponent(BoxCollider);
+  boxCollider.initWithSize(new Vector3(cubeSize, cubeSize, cubeSize));
 
-const sphereCollider = sphereEntity.addComponent(SphereCollider);
-sphereCollider.setSphere(new Vector3(), radius);
+  // create sphere test entity
+  const radius = 1.25;
+  const sphereEntity = rootEntity.createChild("SphereEntity");
+  sphereEntity.transform.setPosition(-2, 0, 0);
 
-class MoveScript extends Script {
-  pos: Vector3 = new Vector3(-5, 0, 0);
-  vel: number = 0.005;
+  const sphereMtl = new BlinnPhongMaterial(engine);
+  const sphereRenderer = sphereEntity.addComponent(MeshRenderer);
+  sphereMtl.baseColor.setValue(Math.random(), Math.random(), Math.random(), 1.0);
+  sphereRenderer.mesh = PrimitiveMesh.createSphere(engine, radius);
+  sphereRenderer.setMaterial(sphereMtl);
 
-  onUpdate(deltaTime: number) {
-    super.onUpdate(deltaTime);
-    if (this.pos.x >= 5 || this.pos.x <= -5) {
-      this.vel *= -1;
+  const sphereCollider = sphereEntity.addComponent(SphereCollider);
+  sphereCollider.initWithRadius(radius);
+
+  class MoveScript extends Script {
+    pos: Vector3 = new Vector3(-5, 0, 0);
+    vel: number = 0.005;
+
+    onUpdate(deltaTime: number) {
+      super.onUpdate(deltaTime);
+      if (this.pos.x >= 5 || this.pos.x <= -5) {
+        this.vel *= -1;
+      }
+      this.pos.x += deltaTime * this.vel;
+
+      this.entity.transform.position = this.pos;
     }
-    this.pos.x += deltaTime * this.vel;
-
-    this.entity.transform.position = this.pos;
-  }
-}
-
-// Collision Detection
-class CollisionScript extends Script {
-  onTriggerExit() {
-    (<BlinnPhongMaterial>sphereRenderer.getMaterial()).baseColor.setValue(Math.random(), Math.random(), Math.random(), 1.0)
   }
 
-  onTriggerEnter() {
-    (<BlinnPhongMaterial>sphereRenderer.getMaterial()).baseColor.setValue(Math.random(), Math.random(), Math.random(), 1.0)
+  // Collision Detection
+  class CollisionScript extends Script {
+    onTriggerExit() {
+      (<BlinnPhongMaterial>sphereRenderer.getMaterial()).baseColor.setValue(Math.random(), Math.random(), Math.random(), 1.0);
+    }
+
+    onTriggerEnter() {
+      (<BlinnPhongMaterial>sphereRenderer.getMaterial()).baseColor.setValue(Math.random(), Math.random(), Math.random(), 1.0);
+    }
   }
-}
 
-sphereEntity.addComponent(CollisionDetection);
-sphereEntity.addComponent(CollisionScript);
-sphereEntity.addComponent(MoveScript);
+  sphereEntity.addComponent(CollisionScript);
+  sphereEntity.addComponent(MoveScript);
 
-// Run engine
-engine.run();
+  // Run engine
+  engine.run();
+
+});
