de4ba8f34ee37b51ea678d6cd49f6aac71edb6e7
eyworldwide <eyworldwide@foxmail.com>
2021-11-18 17:25:53 +0800

    fix typedoc source url

--- src/components/typedoc/Source.tsx
+++ src/components/typedoc/Source.tsx
@@ -5,5 +5,9 @@ import siteConfig from '../../../siteconfig.json';
 
 export default function Source(source: ISource) {
   const url = source.fileName.replace('engine/', siteConfig.engineSource);
+  if (url.endsWith('.d.ts')) {
+    return null;
+  }
+
   return source && <a href={`${url}#L${source.line}`} target="_blank" className="tsc-source"><GithubOutlined /></a>
 }
