b02a02e313e457f4cdd2982ebb6b04992124847d
zhuxudong <callzhuxudong@163.com>
2021-12-29 14:48:43 +0800

    feat: add inline border

--- playground/outline.ts
+++ playground/outline.ts
@@ -69,6 +69,7 @@ engine.resourceManager
   });
 
 /** ------------------ Border ------------------ */
+// 模版测试
 class Border extends Script {
   material: Material;
 
@@ -178,15 +179,108 @@ class Border extends Script {
   }
 }
 
+// 内描边
+class Border2 extends Script {
+  material: Material;
+
+  getBorderMaterial(engine: Engine) {
+    if (!this.material) {
+      if (!Shader.find("border-shader")) {
+        const vertex = `
+                attribute vec3 POSITION;
+                attribute vec3 NORMAL;
+  
+                uniform float u_width;
+                uniform mat4 u_MVPMat;
+                uniform mat4 u_modelMat;
+                uniform mat4 u_viewMat;
+                uniform mat4 u_projMat;
+                uniform mat4 u_normalMat;
+                
+                void main() {
+                   vec4 mPosition = u_modelMat * vec4(POSITION, 1.0);
+                   vec3 mNormal = normalize( mat3(u_normalMat) * NORMAL );
+                   mPosition.xyz += mNormal * u_width;
+                   gl_Position = u_projMat * u_viewMat * mPosition;
+                }
+                `;
+        const fragment = `
+                uniform vec3 u_color;
+
+                void main(){
+                  gl_FragColor = vec4(u_color, 1);
+                }
+                `;
+
+        Shader.create("border-shader", vertex, fragment);
+      }
+      const material = new Material(engine, Shader.find("border-shader"));
+      this.material = material;
+      material.renderQueueType = RenderQueueType.Transparent + 1;
+      material.renderState.rasterState.cullMode = CullMode.Front;
+      material.shaderData.setFloat("u_width", 0.03);
+      material.shaderData.setColor("u_color", new Color(1, 1, 1, 1));
+    }
+
+    return this.material;
+  }
+
+  showBorder(renderer: MeshRenderer) {
+    const entity = renderer.entity;
+    const renderers: MeshRenderer[] = [];
+
+    if (entity.getComponents(MeshRenderer, renderers).length === 1) {
+      const borderMaterial = this.getBorderMaterial(entity.engine);
+      const borderRenderer = entity.addComponent(MeshRenderer);
+      borderRenderer.mesh = renderer.mesh;
+      borderRenderer.setMaterial(borderMaterial);
+      borderRenderer.enabled = true;
+    } else {
+      renderers[0].enabled = true;
+    }
+  }
+
+  hideBorder(renderer: MeshRenderer) {
+    const entity = renderer.entity;
+
+    const renderers: MeshRenderer[] = [];
+    if (entity.getComponents(MeshRenderer, renderers).length === 2) {
+      renderers[0].enabled = false;
+    }
+  }
+
+  onStart() {
+    const framebufferPicker = this.entity.addComponent(FramebufferPicker);
+    framebufferPicker.camera = camera;
+    let lastRenderer = null;
+    framebufferPicker.onPick = (obj) => {
+      setTimeout(() => {
+        if (obj) {
+          const { component } = obj;
+          lastRenderer && this.hideBorder(lastRenderer);
+          this.showBorder(component);
+          lastRenderer = component;
+        } else {
+          lastRenderer && this.hideBorder(lastRenderer);
+        }
+      }, 0);
+    };
+
+    engine.canvas._webCanvas.addEventListener("mousedown", (e) => {
+      framebufferPicker.pick(e.offsetX, e.offsetY);
+    });
+  }
+}
+
 const borderEntity = rootEntity.createChild("border");
-borderEntity.addComponent(Border);
+borderEntity.addComponent(Border2);
 
 const config = {
   plan: "模版测试"
 };
 
 gui
-  .add(config, "plan", ["模版测试", "a"])
+  .add(config, "plan", ["模版测试", "内描边"])
   .onChange((v) => {
     borderEntity.isActive = false;
     if (v === "模版测试") {
