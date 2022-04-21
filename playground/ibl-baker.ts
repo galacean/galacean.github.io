ce4457198d3f3c3e352e019e3b9b7c0f2a517efa
zhuxudong <callzhuxudong@163.com>
2022-04-21 16:16:13 +0800

    refactor: use "HDR-RGBE" for baker

--- playground/ibl-baker.ts
+++ playground/ibl-baker.ts
@@ -56,7 +56,7 @@ Promise.all([
     url: "https://gw.alipayobjects.com/os/bmw-prod/10c5d68d-8580-4bd9-8795-6f1035782b94.bin", // sunset_1K
     // url: "https://gw.alipayobjects.com/os/bmw-prod/20d58ffa-c7da-4c54-8980-4efaf91a0239.bin",// pisa_1K
     // url: "https://gw.alipayobjects.com/os/bmw-prod/59b28d9f-7589-4d47-86b0-52c50b973b10.bin", // footPrint_2K
-    type: AssetType.HDR
+    type: "HDR-RGBE"
   })
 ]).then((textures: TextureCubeMap[]) => {
   const ldrCubeMap = textures[0];
