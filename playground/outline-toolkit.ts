7a95f8bb1a7a79e68b99a407899818e98c79f04f
zhuxudong <callzhuxudong@163.com>
2022-07-20 12:42:55 +0800

    refactor: format code

--- playground/outline-toolkit.ts
+++ playground/outline-toolkit.ts
@@ -3,22 +3,11 @@
  * @category Toolkit
  */
 import * as dat from "dat.gui";
-import {
-  AmbientLight,
-  AssetType,
-  Camera,
-  Entity,
-  GLTFResource,
-  Layer,
-  MeshRenderer,
-  PrimitiveMesh,
-  UnlitMaterial,
-  WebGLEngine
-} from "oasis-engine";
-import { OrbitControl, FramebufferPicker, OutlineManager } from "oasis-engine-toolkit";
-
+import { AmbientLight, AssetType, Camera, GLTFResource, Logger, WebGLEngine, WebGLMode } from "oasis-engine";
+import { FramebufferPicker, OrbitControl, OutlineManager } from "oasis-engine-toolkit";
+Logger.enable();
 const gui = new dat.GUI();
-const engine = new WebGLEngine("canvas");
+const engine = new WebGLEngine("canvas", { webGLMode: WebGLMode.WebGL1 });
 engine.run();
 engine.canvas.resizeByClientSize();
 
@@ -28,16 +17,16 @@ scene.background.solidColor.set(1, 1, 1, 1);
 
 // camera
 const cameraEntity = rootEntity.createChild("camera_node");
-const outlineManager = cameraEntity.addComponent(OutlineManager);
-
 cameraEntity.transform.setPosition(0, 1.3, 10);
+
 const camera = cameraEntity.addComponent(Camera);
 camera.enableFrustumCulling = false;
 cameraEntity.addComponent(OrbitControl).target.set(0, 1.3, 0);
 
+const outlineManager = cameraEntity.addComponent(OutlineManager);
+gui.add(outlineManager, "size", 1, 6, 0.1);
 const framebufferPicker = rootEntity.addComponent(FramebufferPicker);
 framebufferPicker.camera = camera;
-framebufferPicker.colorRenderPass.mask = Layer.Layer0;
 
 document.getElementById("canvas").addEventListener("mousedown", (e) => {
   framebufferPicker.pick(e.offsetX, e.offsetY).then((renderElement) => {
@@ -45,7 +34,7 @@ document.getElementById("canvas").addEventListener("mousedown", (e) => {
     if (renderElement) {
       console.log(renderElement.component.entity);
 
-      outlineManager.addEntity(renderElement.component.entity);
+      outlineManager.addEntity(renderElement.component.entity.parent);
     }
   });
 });
@@ -61,31 +50,19 @@ engine.resourceManager
     ambientLight.specularIntensity = ambientLight.diffuseIntensity = 2;
   });
 
-// engine.resourceManager
-//   .load({
-//     type: AssetType.Prefab,
-//     url: "https://gw.alipayobjects.com/os/OasisHub/440000554/3615/%25E5%25BD%2592%25E6%25A1%25A3.gltf"
-//   })
-//   .then((gltf: GLTFResource) => {
-//     const { defaultSceneRoot } = gltf;
-
-//     for (let i = 0; i < 10; i++) {
-//       const clone = defaultSceneRoot.clone();
-//       clone.transform.setPosition(i - 5, 0, 0);
-//       rootEntity.addChild(clone);
-//     }
-//   });
-
-const mesh = PrimitiveMesh.createSphere(engine);
-const entity = new Entity(engine);
-const renderer = entity.addComponent(MeshRenderer);
-const material = new UnlitMaterial(engine);
-material.baseColor.set(0.5, 0.5, 0, 1);
-renderer.mesh = mesh;
-
-renderer.setMaterial(material);
-for (let i = 0; i < 10; i++) {
-  const clone = entity.clone();
-  clone.transform.setPosition(i - 5, 0, 0);
-  rootEntity.addChild(clone);
-}
+engine.resourceManager
+  .load({
+    type: AssetType.Prefab,
+    // url: "https://gw.alipayobjects.com/os/OasisHub/440000554/3615/%25E5%25BD%2592%25E6%25A1%25A3.gltf"
+    url: "https://gw.alipayobjects.com/os/OasisHub/267000040/9994/%25E5%25BD%2592%25E6%25A1%25A3.gltf"
+  })
+  .then((gltf: GLTFResource) => {
+    const { defaultSceneRoot } = gltf;
+    console.log(gltf);
+    rootEntity.addChild(defaultSceneRoot);
+    for (let i = 0; i < 10; i++) {
+      const clone = defaultSceneRoot.clone();
+      clone.transform.setPosition(i - 5, 0, 0);
+      rootEntity.addChild(clone);
+    }
+  });
