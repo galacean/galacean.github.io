2f6d3cb21ca41789c9330c9ffcf6c1429fc84d43
singlecoder <singlecoder2014@gmail.com>
2022-07-21 10:22:34 +0800

    add text barrage

--- playground/text-barrage.ts
+++ playground/text-barrage.ts
@@ -2,38 +2,155 @@
  * @title Text Barrage
  * @category 2D
  */
+import { Camera, Color, Engine, Entity, Font, Script, TextHorizontalAlignment, TextRenderer, WebGLEngine } from "oasis-engine";
 
-import { Camera, Entity, Font, FontStyle, Script, TextRenderer, Vector3, WebGLEngine } from "oasis-engine";
+const Colors = [
+  new Color(1, 1, 1, 1),
+  new Color(1, 0, 0, 1),
+  new Color(0, 1, 0.89, 1)
+];
+const ColorLastIndex = Colors.length - 1;
+
+const Words = [
+  "Oasis",
+  "oasis",
+  "Hello",
+  "hello",
+  "world",
+  "World",
+  "text",
+  "Text",
+  "ABCabc",
+  "abcDEF",
+  "abcdefg",
+  "hijklmn",
+  "opqrst",
+  "uvwxyz",
+  "ABCDEFG",
+  "HIJKLMN",
+  "OPQRST",
+  "UVWXYZ",
+  "~!@#$",
+  "%^&*",
+  "()_+"
+];
+const WordLastIndex = Words.length - 1;
+
+class TextEntityPool {
+  private _engine: Engine = null;
+  private _elements: Entity[] = [];
+
+  constructor(engine: Engine, count: number) {
+    this._engine = engine;
+    const elements = this._elements;
+    for (let i = 0; i < count; ++i) {
+      const entity = new Entity(engine);
+      elements[i] = entity;
+      entity.addComponent(TextRenderer);
+      entity.addComponent(TextBarrageAnimation);
+    }
+  }
+
+  get(): Entity {
+    if (this._elements.length > 0) {
+      return this._elements.pop();
+    }
+    const entity = new Entity(this._engine);
+    entity.addComponent(TextRenderer);
+    entity.addComponent(TextBarrageAnimation);
+    return entity;
+  }
+
+  put(entity: Entity): void {
+    this._elements.push(entity);
+  }
+}
 
 class TextBarrageAnimation extends Script {
-  static range: number = 0;
-  
-  public isFromLeft: boolean = true;
+  public speed: number = 0;
+  public range: number = 0;
   private _isPlayging: boolean = false;
-  private _speed: number = 0.003;
+  private _cb: Function = null;
 
   onUpdate(dt: number): void {
     if (this._isPlayging) {
-      const { _speed } = this;
-      const curPosX = this.entity.transform.position.x;
-      const diff = this.isFromLeft ? _speed : -_speed;
-      const targetPosX = curPosX + diff * dt;
-      this.entity.transform.setPosition(targetPosX, 0, 0);
-      if (Math.abs(targetPosX) > TextBarrageAnimation.range) {
+      const { position } = this.entity.transform;
+      const curPosX = position.x;
+      const targetPosX = curPosX + this.speed * dt;
+      position.x = targetPosX;
+      if (targetPosX < this.range) {
         this.stop();
+        this._cb && this._cb();
       }
     }
   }
 
   play() {
     this._isPlayging = true;
-    const { range } = TextBarrageAnimation;
-    this.entity.transform.setPosition(this.isFromLeft ? -range : range, 0, 0);
+    return new Promise((resolve) => {
+      this._cb = resolve;
+    });
   }
 
   stop() {
     this._isPlayging = false;
   }
+}
+
+class TextManager extends Script {
+  public halfWidth: number = 0;
+  public halfHeight: number = 0;
+  public cd: number = 5;
+  public curTime: number = 0;
+  
+  private _textCount: number = 0;
+  private _textEntityPool: TextEntityPool = null;
+
+  onAwake(): void {
+    this._textEntityPool = new TextEntityPool(this.engine, 200);
+  }
+
+  onUpdate(deltaTime: number): void {
+    if (this.curTime < this.cd) {
+      this.curTime++;
+      return ;
+    }
+    this.curTime = 0;
+    this._textCount++;
+    const { halfWidth } = this;
+    const entity = this._textEntityPool.get();
+    entity.parent = this.entity;
+    entity.isActive = true;
+    const { position } = entity.transform;
+
+    const textRenderer = entity.getComponent(TextRenderer);
+    textRenderer.text = `${Words[this._getRandomNum(0, WordLastIndex)]} ${Words[this._getRandomNum(0, WordLastIndex)]} ${this._getRandomNum(0, 99)}`;
+    textRenderer.color = Colors[this._getRandomNum(0, ColorLastIndex)];
+    textRenderer.font = Font.createFromOS(entity.engine, "Arial");
+    textRenderer.fontSize = 36;
+    textRenderer.priority = this._textCount;
+    textRenderer.horizontalAlignment = TextHorizontalAlignment.Right;
+    const bounds = textRenderer.bounds;
+    const textWidth = bounds.max.x - bounds.min.x;
+    position.x = halfWidth + textWidth;
+    const limitHeight = halfHeight * 100;
+    position.y = this._getRandomNum(-limitHeight, limitHeight) * 0.01;
+
+    const textScript = entity.getComponent(TextBarrageAnimation);
+    textScript.speed = -0.003 + this._getRandomNum(-200, 100) * 0.00001;
+    textScript.range = -halfWidth;
+    textScript.play().then(() => {
+      entity.parent = null;
+      entity.isActive = false;
+      this._textEntityPool.put(entity);
+    });
+  }
+
+  private _getRandomNum(min: number, max: number): number {
+		const range = max - min;
+		const rand = Math.random();
+		return (min + Math.round(rand * range));
+	}
 }
 
 // Create engine object
@@ -49,42 +166,9 @@ const cameraEntity = rootEntity.createChild("camera_entity");
 cameraEntity.transform.setPosition(0, 0, 10);
 const camera = cameraEntity.addComponent(Camera);
 camera.isOrthographic = true;
-TextBarrageAnimation.range = camera.orthographicSize * camera.aspectRatio;
 
-//
-const textEntity = createText();
-const textScript = textEntity.addComponent(TextBarrageAnimation);
-textScript.play();
+const textManager = rootEntity.addComponent(TextManager);
+const halfHeight = camera.orthographicSize;
+textManager.halfWidth = halfHeight * camera.aspectRatio;
+textManager.halfHeight = halfHeight;
 
-/**
- * Create text to display by params.
- * @param fontFamily - The font family
- * @param fontSize - The size of font
- * @param bold - The text whether bold
- * @param italic - The text whether italic
- */
-function createText(
-  fontFamily: string = "Arial",
-  fontSize: number = 36,
-  bold: boolean = false,
-  italic: boolean = false
-): Entity {
-  // Create text entity
-  const entity = rootEntity.createChild("text");
-  entity.transform.position = new Vector3();
-  // Add text renderer for text entity
-  const renderer = entity.addComponent(TextRenderer);
-  // // Set text color
-  // renderer.color = color;
-  // Set text to render
-  renderer.text = "Hello World";
-  // Set font with font family
-  renderer.font = Font.createFromOS(entity.engine, fontFamily);
-  // Set font size
-  renderer.fontSize = fontSize;
-  // Set font whether bold
-  bold && (renderer.fontStyle |= FontStyle.Bold);
-  // Set font whether italic
-  italic && (renderer.fontStyle |= FontStyle.Italic);
-  return entity;
-}
