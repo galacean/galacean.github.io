040ba9521ce007099f3e74bc37b7bb48ae71dd73
zhuxudong <callzhuxudong@163.com>
2022-07-25 15:12:32 +0800

    Playground: skeleton helper (#411)
    * feat: add toolkit

---
+++ playground/skeleton-viewer.ts
@@ -0,0 +1,34 @@
+/**
+ * @title Skeleton Viewer
+ * @category Toolkit
+ */
+import { Animator, Camera, GLTFResource, Vector3, WebGLEngine } from "oasis-engine";
+import { OrbitControl, SkeletonViewer } from "oasis-engine-toolkit";
+
+const engine = new WebGLEngine("canvas");
+engine.canvas.resizeByClientSize();
+const scene = engine.sceneManager.activeScene;
+const rootNode = scene.createRootEntity();
+scene.ambientLight.diffuseSolidColor.set(1, 1, 1, 1);
+
+// Create camera
+const cameraEntity = rootNode.createChild("camera_node");
+cameraEntity.transform.position = new Vector3(10, 10, 30);
+cameraEntity.addComponent(Camera);
+const control = cameraEntity.addComponent(OrbitControl);
+control.target.set(0, 3, 0);
+
+engine.resourceManager
+  .load<GLTFResource>("https://gw.alipayobjects.com/os/bmw-prod/f40ef8dd-4c94-41d4-8fac-c1d2301b6e47.glb")
+  .then((gltf) => {
+    const { defaultSceneRoot, animations } = gltf;
+    const animator = defaultSceneRoot.getComponent(Animator);
+    defaultSceneRoot.transform.setScale(0.1, 0.1, 0.1);
+    rootNode.addChild(defaultSceneRoot);
+    animator.play(animations[1].name);
+
+    defaultSceneRoot.addComponent(SkeletonViewer);
+  });
+
+// Run
+engine.run();
