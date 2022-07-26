7fbad2bed9a30ad791ef657072842575bfa44bc1
singlecoder <singlecoder2014@gmail.com>
2022-07-26 11:32:17 +0800

    opt code

--- playground/text-barrage.ts
+++ playground/text-barrage.ts
@@ -1,100 +1,74 @@
 /**
  * @title Text Barrage
- * @category 2D
+ * @category Benchmark
  */
-import { Camera, Color, Font, Script, TextHorizontalAlignment, TextRenderer, WebGLEngine } from "oasis-engine";
-
-const Colors = [new Color(1, 1, 1, 1), new Color(1, 0, 0, 1), new Color(0, 1, 0.89, 1)];
-const ColorLastIndex = Colors.length - 1;
-
-const Words = [
-  "OASIS",
-  "oasis",
-  "HELLO",
-  "hello",
-  "WORLD",
-  "world",
-  "TEXT",
-  "text",
-  "PEACE",
-  "peace",
-  "LOVE",
-  "love",
-  "abcdefg",
-  "hijklmn",
-  "opqrst",
-  "uvwxyz",
-  "ABCDEFG",
-  "HIJKLMN",
-  "OPQRST",
-  "UVWXYZ",
-  "~!@#$",
-  "%^&*",
-  "()_+"
-];
-const WordLastIndex = Words.length - 1;
-
-function getRandomNum(min: number, max: number): number {
-  const range = max - min;
-  const rand = Math.random();
-  return min + Math.round(rand * range);
-}
+import { Camera, Color, Engine, Font, Script, TextHorizontalAlignment, TextRenderer, WebGLEngine } from "oasis-engine";
+import { Stats } from "oasis-engine-toolkit";
+Engine.registerFeature(Stats);
 
 class TextBarrageAnimation extends Script {
-  public halfWidth: number = 0;
-  public halfHeight: number = 0;
-  public textCount: number = 0;
-  public speed: number = 0;
-  public range: number = 0;
-  public delayFrame: number = 0;
-  private _curFrame: number = 0;
+  // prettier-ignore
+  static words = [ "OASIS", "oasis", "HELLO", "hello", "WORLD", "world", "TEXT", "text", "PEACE", "peace", "LOVE", "love", "abcdefg", "hijklmn", "opqrst", "uvwxyz", "ABCDEFG", "HIJKLMN", "OPQRST", "UVWXYZ", "~!@#$", "%^&*", "()_+" ];
+  static colors = [new Color(1, 1, 1, 1), new Color(1, 0, 0, 1), new Color(0, 1, 0.89, 1)];
+
+  public camera: Camera;
+  public priorityOffset: number = 0;
+
+  private _speed: number = 0;
+  private _range: number = 0;
   private _isPlayging: boolean = false;
   private _textRenderer: TextRenderer = null;
-
-  onStart(): void {
-    this._textRenderer = this.entity.getComponent(TextRenderer);
-    this.reset();
-  }
-
-  onUpdate(dt: number): void {
-    if (this._isPlayging) {
-      const { position } = this.entity.transform;
-      const curPosX = position.x;
-      const targetPosX = curPosX + this.speed * dt;
-      position.x = targetPosX;
-      if (targetPosX < this.range) {
-        this.reset();
-      }
-    } else {
-      this._curFrame++;
-      if (this._curFrame >= this.delayFrame) {
-        this.play();
-      }
-    }
-  }
 
   play() {
     this._isPlayging = true;
   }
 
-  reset() {
-    const { entity } = this;
-    const { position } = entity.transform;
+  onStart(): void {
+    this._textRenderer = this.entity.getComponent(TextRenderer);
+    this._range = -camera.orthographicSize * camera.aspectRatio;
+    this._reset(true);
+  }
+
+  onUpdate(dt: number): void {
+    if (this._isPlayging) {
+      const { position } = this.entity.transform;
+      position.x += this._speed * dt;
+      if (position.x < this._range) {
+        this._reset(false);
+      }
+    }
+  }
+
+  private _reset(isFirst: boolean) {
     const textRenderer = this._textRenderer;
-    // Reset priority for renderer.
-    textRenderer.priority += this.textCount;
-    // Reset the text to render.
-    textRenderer.text = `${Words[getRandomNum(0, WordLastIndex)]} ${
-      Words[getRandomNum(0, WordLastIndex)]
+    const { words, colors } = TextBarrageAnimation;
+
+    // Reset priority for renderer
+    textRenderer.priority += this.priorityOffset;
+
+    // Reset the text to render
+    const wordLastIndex = words.length - 1;
+    textRenderer.text = `${words[getRandomNum(0, wordLastIndex)]} ${
+      words[getRandomNum(0, wordLastIndex)]
     } ${getRandomNum(0, 99)}`;
-    // Reset position.
-    const bounds = textRenderer.bounds;
-    const textWidth = bounds.max.x - bounds.min.x;
-    position.x = this.halfWidth + textWidth;
-    const limitHeight = this.halfHeight * 100;
-    position.y = getRandomNum(-limitHeight, limitHeight) * 0.01;
-    // Reset speed.
-    this.speed = -0.003 + getRandomNum(-200, 100) * 0.00001;
+
+    // Reset color
+    textRenderer.color = colors[getRandomNum(0, colors.length - 1)];
+
+    // Reset position
+    const { position } = this.entity.transform;
+    const { orthographicSize } = camera;
+    if (isFirst) {
+      const halfOrthoWidth = orthographicSize * camera.aspectRatio;
+      position.x = getRandomNum(-halfOrthoWidth, halfOrthoWidth);
+    } else {
+      const { bounds } = textRenderer;
+      position.x = orthographicSize * camera.aspectRatio + bounds.max.x - bounds.min.x;
+    }
+    position.y = getRandomNum(-orthographicSize, orthographicSize);
+
+    // Reset speed
+    this._speed = getRandomNum(-500, -200) * 0.00001;
   }
 }
 
@@ -108,28 +82,31 @@ const rootEntity = scene.createRootEntity();
 
 // Create camera
 const cameraEntity = rootEntity.createChild("camera_entity");
-cameraEntity.transform.setPosition(0, 0, 10);
 const camera = cameraEntity.addComponent(Camera);
+cameraEntity.transform.setPosition(0, 0, 10);
 camera.isOrthographic = true;
 
 // Create text barrage.
-const halfHeight = camera.orthographicSize;
-const halfWidth = halfHeight * camera.aspectRatio;
 const textCount = 50;
 for (let i = 0; i < textCount; ++i) {
   const textEntity = rootEntity.createChild();
+
   // Init text renderer.
   const textRenderer = textEntity.addComponent(TextRenderer);
-  textRenderer.color = Colors[getRandomNum(0, ColorLastIndex)];
   textRenderer.font = Font.createFromOS(engine, "Arial");
   textRenderer.fontSize = 36;
   textRenderer.priority = i;
   textRenderer.horizontalAlignment = TextHorizontalAlignment.Right;
+
   // Init and reset text barrage animation.
   const barrage = textEntity.addComponent(TextBarrageAnimation);
-  barrage.halfWidth = halfWidth;
-  barrage.halfHeight = halfHeight;
-  barrage.textCount = textCount;
-  barrage.range = -halfWidth;
-  barrage.delayFrame = i * 5;
+  barrage.camera = camera;
+  barrage.priorityOffset = textCount;
+  barrage.play();
+}
+
+function getRandomNum(min: number, max: number): number {
+  const range = max - min;
+  const rand = Math.random();
+  return min + Math.round(rand * range);
 }
