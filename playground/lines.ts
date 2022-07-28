27616e76f7cedbae01b8ca2e26d5ba75cf466803
izzy.cz <izzy.cz@antfin.com>
2022-07-28 14:41:36 +0800

    fix: cr

--- playground/lines.ts
+++ playground/lines.ts
@@ -1,6 +1,6 @@
 /**
  * @title Lines
- * @category 2D
+ * @category Toolkit
  */
  import {
   Camera, Color,
@@ -11,47 +11,24 @@
 import { Line, DashLine, LineCap, LineJoin } from "oasis-engine-toolkit";
 import * as dat from 'dat.gui';
 
-const gui = new dat.GUI({
-	name: 'line'
-});
+const lines: Line[] = [];
 const controls = {
 	width: 0.02,
 	cap: LineCap.Round,
 	join: LineJoin.Round,
 }
-const widthControl = gui.add(controls, 'width', 0.01, 0.1);
-const capControl = gui.add(controls, 'cap', { butt: LineCap.Butt, round: LineCap.Round, square: LineCap.Square});
-const joinControl = gui.add(controls, 'join', { bevel: LineJoin.Bevel, round: LineJoin.Round, miter: LineJoin.Miter});
-
-widthControl.onChange(change)
-capControl.onChange(change)
-joinControl.onChange(change)
-
-const lines: Line[] = [];
 const colors = [
 	new Color(91 / 255, 143 / 255, 249 / 255), 
 	new Color(92 / 255, 206 / 255, 161 / 255),
 	new Color(246 / 255, 189 / 255, 22 / 255),
 	new Color(170 / 255, 0 / 255, 97 / 255),
 	new Color(0 / 255, 155 / 255, 119 / 255)
-
 ]
-function change() {
-	if(lines.length) {
-		lines.forEach(line => {
-			line.cap = controls.cap;
-			line.join = Number(controls.join);
-			line.width = Number(controls.width);
-		})
-	}
-}
-
 // Create engine and get root entity.
 const engine = new WebGLEngine("canvas");
 engine.canvas.resizeByClientSize();
 
 const rootEntity = engine.sceneManager.activeScene.createRootEntity("Root");
-
 
 // Create camera.
 const cameraEntity = rootEntity.createChild("Camera");
@@ -60,8 +37,7 @@ cameraEntity.transform.setPosition(center.x, center.y, 50);
 cameraEntity.transform.lookAt(center);
 const camera = cameraEntity.addComponent(Camera);
 camera.isOrthographic = true;
-engine.sceneManager.activeScene.background.solidColor = new Color(0.8, 0.8, 0.8, 1);
-
+engine.sceneManager.activeScene.background.solidColor.set(0.8, 0.8, 0.8, 1);
 engine.run();
 
 fetch(
@@ -90,3 +66,29 @@ fetch(
 	}
 })
 
+showDebug();
+
+function showDebug() {
+	const gui = new dat.GUI({
+		name: 'line'
+	});
+
+	const widthControl = gui.add(controls, 'width', 0.01, 0.1);
+	const capControl = gui.add(controls, 'cap', { butt: LineCap.Butt, round: LineCap.Round, square: LineCap.Square});
+	const joinControl = gui.add(controls, 'join', { bevel: LineJoin.Bevel, round: LineJoin.Round, miter: LineJoin.Miter});
+
+	widthControl.onChange(change)
+	capControl.onChange(change)
+	joinControl.onChange(change)
+
+	
+	function change() {
+		if(lines.length) {
+			lines.forEach(line => {
+				line.cap = controls.cap;
+				line.join = Number(controls.join);
+				line.width = Number(controls.width);
+			})
+		}
+	}
+}
