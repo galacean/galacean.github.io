4ece47bd7ac6ba28fed016257294b803ad9fdc72
eyworldwide <eyworldwide@foxmail.com>
2022-08-01 22:02:30 +0800

    chore: v0.8

--- copy.js
+++ copy.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 
-fs.removeSync('./0.7'); 
+fs.removeSync('./0.8'); 
 fs.removeSync('./404'); 
 fs.removeSync('./examples'); 
 fs.removeSync('./gltf-viewer'); 
@@ -8,7 +8,7 @@ fs.removeSync('./404.html');
 fs.removeSync('./index.html'); 
 fs.removeSync('./sitemap.xml'); 
 
-fs.copySync('./public/0.7', './0.7');
+fs.copySync('./public/0.8', './0.8');
 fs.copySync('./public/404', './404'); 
 fs.copySync('./public/examples', './examples'); 
 fs.copySync('./public/gltf-viewer', './gltf-viewer'); 
