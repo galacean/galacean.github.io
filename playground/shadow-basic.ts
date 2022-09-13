9abb947947e0c26780a726e9f900db4819eb4b53
yangfengzzz <yangfengzzz@hotmail.com>
2022-09-13 17:15:09 +0800

    add shadow docs

--- playground/shadow-basic.ts
+++ playground/shadow-basic.ts
@@ -4,7 +4,17 @@
  */
 
 import {FreeControl} from "@oasis-engine-toolkit/controls";
-import {Camera, GLTFResource, WebGLEngine, Vector3, DirectLight, Color, MeshRenderer} from "oasis-engine";
+import {
+  Camera,
+  GLTFResource,
+  WebGLEngine,
+  Vector3,
+  DirectLight,
+  Color,
+  MeshRenderer,
+  AmbientLight,
+  AssetType
+} from "oasis-engine";
 
 const engine = new WebGLEngine("canvas");
 engine.canvas.resizeByClientSize();
@@ -16,16 +26,18 @@ const rootEntity = scene.createRootEntity();
 const cameraEntity = rootEntity.createChild("camera");
 cameraEntity.transform.setPosition(3, 2, 0.5);
 cameraEntity.transform.lookAt(new Vector3(0, 2, 0.5));
-cameraEntity.addComponent(Camera);
-cameraEntity.transform.setPosition(3, 3, 3);
-cameraEntity.addComponent(FreeControl);
+const camera = cameraEntity.addComponent(Camera);
+camera.enableFrustumCulling = false;
+cameraEntity.addComponent(FreeControl).floorMock = false;
 
 // Create an entity to add light component
 const lightEntity = rootEntity.createChild("light");
-lightEntity.transform.setRotation(45, 45, 45);
+lightEntity.transform.setPosition(3, 2, 0.5);
+lightEntity.transform.lookAt(new Vector3(0, 2, 0.5));
 const directLight = lightEntity.addComponent(DirectLight);
 directLight.color = new Color(1.0, 1.0, 1.0);
 directLight.enableShadow = true;
+directLight.shadowBias = 5;
 
 engine.resourceManager
   .load<GLTFResource>("https://gw.alipayobjects.com/os/bmw-prod/ca50859b-d736-4a3e-9fc3-241b0bd2afef.gltf")
@@ -40,5 +52,13 @@ engine.resourceManager
       renderer.castShadows = true;
     }
 
-    engine.run();
+    engine.resourceManager
+      .load<AmbientLight>({
+        type: AssetType.Env,
+        url: "https://gw.alipayobjects.com/os/bmw-prod/09904c03-0d23-4834-aa73-64e11e2287b0.bin"
+      })
+      .then((ambientLight) => {
+        scene.ambientLight = ambientLight;
+        engine.run();
+      })
   });
