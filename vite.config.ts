1452954a5afd4aa3349e760c05035528c90cfe37
eyworldwide <eyworldwide@foxmail.com>
2022-10-18 11:18:10 +0800

    chore: add server property in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
   return defineConfig({
+    server: {
+      open: true,
+      host: "0.0.0.0",
+      port: 4000
+    },
     plugins: [react()],
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
