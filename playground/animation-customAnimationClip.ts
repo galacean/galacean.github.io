8bd9267d7cf9b81ba9fa13ba5160a2b0ebd3ccbd
luzhuang <364439895@qq.com>
2022-11-02 11:24:54 +0800

    feat: opt custom animationClip

--- playground/animation-customAnimationClip.ts
+++ playground/animation-customAnimationClip.ts
@@ -2,12 +2,12 @@
  * @title Animation CustomAnimationClip
  * @category Animation
  */
-import { OrbitControl } from "@oasis-engine-toolkit/controls";
 import * as dat from "dat.gui";
 import {
   AnimationClip,
   AnimationColorCurve,
   AnimationEvent,
+  AnimationFloatCurve,
   AnimationVector3Curve,
   Animator,
   AnimatorController,
@@ -36,16 +36,27 @@ const scene = engine.sceneManager.activeScene;
 const rootEntity = scene.createRootEntity();
 
 // camera
-const cameraEntity = rootEntity.createChild("camera_node");
-cameraEntity.transform.position = new Vector3(0, 1, 5);
+const cameraWrap = rootEntity.createChild("camera_wrap");
+const cameraEntity = cameraWrap.createChild("camera");
+cameraEntity.transform.position = new Vector3(0, 3, 8);
+cameraEntity.transform.rotation = new Vector3(-2, 0, 0);
 cameraEntity.addComponent(Camera);
-cameraEntity.addComponent(OrbitControl).target = new Vector3(0, 1, 0);
 
 const lightNode = rootEntity.createChild("light_node");
-const light = lightNode.addComponent(DirectLight)
+const light = lightNode.addComponent(DirectLight);
 light.intensity = 0.6;
 lightNode.transform.lookAt(new Vector3(0, 0, 1));
 lightNode.transform.rotate(new Vector3(0, 90, 0));
+
+engine.resourceManager
+  .load<GLTFResource>(
+    "https://gw.alipayobjects.com/os/bmw-prod/ca50859b-d736-4a3e-9fc3-241b0bd2afef.gltf"
+  )
+  .then((gltfResource) => {
+    const { defaultSceneRoot } = gltfResource;
+    defaultSceneRoot.transform.setScale(0.05, 0.05, 0.05);
+    rootEntity.addChild(defaultSceneRoot);
+  });
 
 engine.resourceManager
   .load<GLTFResource>(
@@ -57,74 +68,101 @@ engine.resourceManager
     const animator = defaultSceneRoot.getComponent(Animator);
 
     //custom rotate clip
-    const rotateClip = new AnimationClip('rotate');
-    const rotateState = animator.animatorController.layers[0].stateMachine.addState('rotate');
+    const rotateClip = new AnimationClip("rotate");
+    const rotateState =
+      animator.animatorController.layers[0].stateMachine.addState("rotate");
     rotateState.clip = rotateClip;
-
     const rotateCurve = new AnimationVector3Curve();
     const key1 = new Keyframe<Vector3>();
     key1.time = 0;
-    key1.value = new Vector3(0,0,0);
+    key1.value = new Vector3(0, 0, 0);
     const key2 = new Keyframe<Vector3>();
-    key2.time = 10;
-    key2.value = new Vector3(0,360,0);
+    key2.time = 15;
+    key2.value = new Vector3(0, 360, 0);
     rotateCurve.addKey(key1);
     rotateCurve.addKey(key2);
-    rotateClip.addCurveBinding('', Transform, "rotation", rotateCurve)
-                           
+    rotateClip.addCurveBinding("", Transform, "rotation", rotateCurve);
 
     //custom light animation
-    const lightAnimator = lightNode.addComponent(Animator);
-    lightAnimator.animatorController = new AnimatorController();
-    const layer = new AnimatorControllerLayer('base');
-    lightAnimator.animatorController.addLayer(layer);
-    const stateMachine = layer.stateMachine = new AnimatorStateMachine();
-    const colorState = stateMachine.addState('colorAnim');
-    const colorClip = colorState.clip = new AnimationClip('color');
+    const sceneAnimator = rootEntity.addComponent(Animator);
+    sceneAnimator.animatorController = new AnimatorController();
+    const layer = new AnimatorControllerLayer("base");
+    sceneAnimator.animatorController.addLayer(layer);
+    const stateMachine = (layer.stateMachine = new AnimatorStateMachine());
+    const sceneState = stateMachine.addState("sceneAnim");
+    const sceneClip = (sceneState.clip = new AnimationClip("color"));
 
     const colorCurve = new AnimationColorCurve();
     const key3 = new Keyframe<Color>();
     key3.time = 0;
-    key3.value = new Color(1,0,0,1);
+    key3.value = new Color(1, 0, 0, 1);
     const key4 = new Keyframe<Color>();
     key4.time = 5;
-    key4.value =new Color(0,1,0,1);
-    const key5= new Keyframe<Color>();
+    key4.value = new Color(0, 1, 0, 1);
+    const key5 = new Keyframe<Color>();
     key5.time = 10;
-    key5.value =new Color(0,1,0,1);
-    const key6= new Keyframe<Color>();
+    key5.value = new Color(0, 1, 0, 1);
+    const key6 = new Keyframe<Color>();
     key6.time = 15;
-    key6.value =new Color(1,0,0,1);
+    key6.value = new Color(1, 0, 0, 1);
     colorCurve.addKey(key3);
     colorCurve.addKey(key4);
     colorCurve.addKey(key5);
     colorCurve.addKey(key6);
-    colorClip.addCurveBinding('', DirectLight, 'color', colorCurve)
-    // curve can be reused
-    colorClip.addCurveBinding('', Transform, "rotation", rotateCurve)
 
-    lightAnimator.play('colorAnim', 0)
+    const fovCurve = new AnimationFloatCurve();
+    const key7 = new Keyframe<number>();
+    key7.time = 0;
+    key7.value = 45;
+    const key8 = new Keyframe<number>();
+    key8.time = 8;
+    key8.value = 80;
+    const key9 = new Keyframe<number>();
+    key9.time = 15;
+    key9.value = 45;
+
+    fovCurve.addKey(key7);
+    fovCurve.addKey(key8);
+    fovCurve.addKey(key9);
+
+    sceneClip.addCurveBinding("/light_node", DirectLight, "color", colorCurve);
+    // curve can be reused
+    sceneClip.addCurveBinding(
+      "/camera_wrap",
+      Transform,
+      "rotation",
+      rotateCurve
+    );
+    sceneClip.addCurveBinding(
+      "/camera_wrap/camera",
+      Camera,
+      "fieldOfView",
+      fovCurve
+    );
+
+    sceneAnimator.play("sceneAnim", 0);
     animator.play("rotate", 0);
 
     initDatGUI(animator, animations);
   });
 
-  engine.run();
+engine.run();
 
-  const initDatGUI = (animator, animations) => {
-    const animationNames = animations.filter((clip) => !clip.name.includes("pose")).map((clip) => clip.name);
-    animationNames.unshift('rotate');
-    const debugInfo = {
-      animation: animationNames[0],
-      speed: 1
-    };
-  
-    gui.add(debugInfo, "animation", animationNames).onChange((v) => {
-      animator.crossFade(v, 0.3);
-    });
-  
-    gui.add(debugInfo, "speed", -1, 1).onChange((v) => {
-      animator.speed = v;
-    });
-  }
-  
+const initDatGUI = (animator, animations) => {
+  const animationNames = animations
+    .filter((clip) => !clip.name.includes("pose"))
+    .map((clip) => clip.name);
+  animationNames.unshift("rotate");
+  const debugInfo = {
+    animation: animationNames[0],
+    speed: 1,
+  };
+
+  gui.add(debugInfo, "animation", animationNames).onChange((v) => {
+    animator.crossFade(v, 0.3);
+  });
+
+  gui.add(debugInfo, "speed", -1, 1).onChange((v) => {
+    animator.speed = v;
+  });
+};
