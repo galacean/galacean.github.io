66e88d1d15a5eb83934b32c1ba75f0eb20981334
luzhuang <364439895@qq.com>
2022-11-04 11:29:55 +0800

    feat: opt custom animationClip demo

--- playground/animation-customAnimationClip.ts
+++ playground/animation-customAnimationClip.ts
@@ -2,7 +2,6 @@
  * @title Animation CustomAnimationClip
  * @category Animation
  */
-import * as dat from "dat.gui";
 import {
   AnimationClip,
   AnimationColorCurve,
@@ -20,13 +19,13 @@ import {
   Keyframe,
   Logger,
   Script,
+  SpotLight,
   SystemInfo,
   TextRenderer,
   Transform,
   Vector3,
   WebGLEngine,
 } from "oasis-engine";
-const gui = new dat.GUI();
 
 Logger.enable();
 const engine = new WebGLEngine("canvas");
@@ -42,11 +41,29 @@ cameraEntity.transform.position = new Vector3(0, 3, 8);
 cameraEntity.transform.rotation = new Vector3(-2, 0, 0);
 cameraEntity.addComponent(Camera);
 
-const lightNode = rootEntity.createChild("light_node");
-const light = lightNode.addComponent(DirectLight);
-light.intensity = 0.6;
-lightNode.transform.lookAt(new Vector3(0, 0, 1));
-lightNode.transform.rotate(new Vector3(0, 90, 0));
+const lightWrap = rootEntity.createChild("light_wrap");
+
+const directLightEntity = lightWrap.createChild("light_node");
+const directLight = directLightEntity.addComponent(DirectLight);
+directLight.intensity = 0.6;
+directLightEntity.transform.lookAt(new Vector3(0, 0, 1));
+directLightEntity.transform.rotate(new Vector3(0, 90, 0));
+
+const spotLightEntity = lightWrap.createChild("spotLight1");
+const spotLightEntity2 = spotLightEntity.clone();
+spotLightEntity2.name = "spotLight2";
+spotLightEntity2.transform.setRotation(-120, 0, 0);
+lightWrap.addChild(spotLightEntity2);
+
+const spotLight = spotLightEntity.addComponent(SpotLight);
+spotLight.angle = Math.PI / 60;
+spotLightEntity.transform.setPosition(0, 8, 0);
+spotLightEntity.transform.setRotation(-60, 0, 0);
+const spotLight2 = spotLightEntity2.addComponent(SpotLight);
+spotLight2.angle = Math.PI / 60;
+spotLightEntity2.transform.setPosition(0, 8, 0);
+spotLightEntity2.transform.setRotation(-60, 0, 0);
+
 
 engine.resourceManager
   .load<GLTFResource>(
@@ -60,18 +77,22 @@ engine.resourceManager
 
 engine.resourceManager
   .load<GLTFResource>(
-    "https://gw.alipayobjects.com/os/bmw-prod/5e3c1e4e-496e-45f8-8e05-f89f2bd5e4a4.glb"
+    "https://gw.alipayobjects.com/os/OasisHub/244228a7-361c-4c63-a790-dd9e19d12e78/data.gltf"
   )
   .then((gltfResource) => {
-    const { defaultSceneRoot, animations } = gltfResource;
+    const { defaultSceneRoot, animations = [] } = gltfResource;
     rootEntity.addChild(defaultSceneRoot);
     const animator = defaultSceneRoot.getComponent(Animator);
 
-    //custom rotate clip
-    const rotateClip = new AnimationClip("rotate");
-    const rotateState =
-      animator.animatorController.layers[0].stateMachine.addState("rotate");
-    rotateState.clip = rotateClip;
+    const sceneAnimator = rootEntity.addComponent(Animator);
+    sceneAnimator.animatorController = new AnimatorController();
+    const layer = new AnimatorControllerLayer("base");
+    sceneAnimator.animatorController.addLayer(layer);
+    const stateMachine = (layer.stateMachine = new AnimatorStateMachine());
+    const sceneState = stateMachine.addState("sceneAnim");
+    const sceneClip = (sceneState.clip = new AnimationClip("sceneAnim"));
+
+    //custom rotate curve
     const rotateCurve = new AnimationVector3Curve();
     const key1 = new Keyframe<Vector3>();
     key1.time = 0;
@@ -81,17 +102,8 @@ engine.resourceManager
     key2.value = new Vector3(0, 360, 0);
     rotateCurve.addKey(key1);
     rotateCurve.addKey(key2);
-    rotateClip.addCurveBinding("", Transform, "rotation", rotateCurve);
 
-    //custom light animation
-    const sceneAnimator = rootEntity.addComponent(Animator);
-    sceneAnimator.animatorController = new AnimatorController();
-    const layer = new AnimatorControllerLayer("base");
-    sceneAnimator.animatorController.addLayer(layer);
-    const stateMachine = (layer.stateMachine = new AnimatorStateMachine());
-    const sceneState = stateMachine.addState("sceneAnim");
-    const sceneClip = (sceneState.clip = new AnimationClip("color"));
-
+    //custom color curve
     const colorCurve = new AnimationColorCurve();
     const key3 = new Keyframe<Color>();
     key3.time = 0;
@@ -101,7 +113,7 @@ engine.resourceManager
     key4.value = new Color(0, 1, 0, 1);
     const key5 = new Keyframe<Color>();
     key5.time = 10;
-    key5.value = new Color(0, 1, 0, 1);
+    key5.value = new Color(0, 0, 1, 1);
     const key6 = new Keyframe<Color>();
     key6.time = 15;
     key6.value = new Color(1, 0, 0, 1);
@@ -110,6 +122,25 @@ engine.resourceManager
     colorCurve.addKey(key5);
     colorCurve.addKey(key6);
 
+    const color2Curve = new AnimationColorCurve();
+    const key16 = new Keyframe<Color>();
+    key16.time = 0;
+    key16.value = new Color(0, 0, 1, 1);
+    const key17 = new Keyframe<Color>();
+    key17.time = 5;
+    key17.value = new Color(0, 1, 0, 1);
+    const key18 = new Keyframe<Color>();
+    key18.time = 10;
+    key18.value = new Color(1, 0, 0, 1);
+    const key19 = new Keyframe<Color>();
+    key19.time = 15;
+    key19.value = new Color(0, 0, 1, 1);
+    color2Curve.addKey(key16);
+    color2Curve.addKey(key17);
+    color2Curve.addKey(key18);
+    color2Curve.addKey(key19);
+
+    //custom fov curve
     const fovCurve = new AnimationFloatCurve();
     const key7 = new Keyframe<number>();
     key7.time = 0;
@@ -125,7 +156,66 @@ engine.resourceManager
     fovCurve.addKey(key8);
     fovCurve.addKey(key9);
 
-    sceneClip.addCurveBinding("/light_node", DirectLight, "color", colorCurve);
+    //custom spotLight1 rotate curve
+    const spotLight1RotateCurve = new AnimationVector3Curve();
+    const key10 = new Keyframe<Vector3>();
+    key10.time = 0;
+    key10.value = new Vector3(-60, 0, 0);
+    const key11 = new Keyframe<Vector3>();
+    key11.time = 10;
+    key11.value = new Vector3(-120, 0, 0);
+    const key12 = new Keyframe<Vector3>();
+    key12.time = 15;
+    key12.value = new Vector3(-60, 0, 0);
+    spotLight1RotateCurve.addKey(key10);
+    spotLight1RotateCurve.addKey(key11);
+    spotLight1RotateCurve.addKey(key12);
+
+    //custom spotLight2 rotate curve
+    const spotLight2RotateCurve = new AnimationVector3Curve();
+    const key13 = new Keyframe<Vector3>();
+    key13.time = 0;
+    key13.value = new Vector3(-120, 0, 0);
+    const key14 = new Keyframe<Vector3>();
+    key14.time = 10;
+    key14.value = new Vector3(-60, 0, 0);
+    const key15 = new Keyframe<Vector3>();
+    key15.time = 15;
+    key15.value = new Vector3(-120, 0, 0);
+    spotLight2RotateCurve.addKey(key13);
+    spotLight2RotateCurve.addKey(key14);
+    spotLight2RotateCurve.addKey(key15);
+
+    sceneClip.addCurveBinding(
+      "/light_wrap/spotLight1",
+      SpotLight,
+      "color",
+      colorCurve
+    );
+    sceneClip.addCurveBinding(
+      "/light_wrap/spotLight1",
+      Transform,
+      "rotation",
+      spotLight1RotateCurve
+    );
+    sceneClip.addCurveBinding(
+      "/light_wrap/spotLight2",
+      Transform,
+      "rotation",
+      spotLight2RotateCurve
+    );
+    sceneClip.addCurveBinding(
+      "/light_wrap/spotLight2",
+      SpotLight,
+      "color",
+      color2Curve
+    );
+    sceneClip.addCurveBinding(
+      "/light_wrap",
+      Transform,
+      "rotation",
+      rotateCurve
+    );
     // curve can be reused
     sceneClip.addCurveBinding(
       "/camera_wrap",
@@ -141,28 +231,7 @@ engine.resourceManager
     );
 
     sceneAnimator.play("sceneAnim", 0);
-    animator.play("rotate", 0);
-
-    initDatGUI(animator, animations);
+    animator.play(animations[0].name, 0);
   });
 
 engine.run();
-
-const initDatGUI = (animator, animations) => {
-  const animationNames = animations
-    .filter((clip) => !clip.name.includes("pose"))
-    .map((clip) => clip.name);
-  animationNames.unshift("rotate");
-  const debugInfo = {
-    animation: animationNames[0],
-    speed: 1,
-  };
-
-  gui.add(debugInfo, "animation", animationNames).onChange((v) => {
-    animator.crossFade(v, 0.3);
-  });
-
-  gui.add(debugInfo, "speed", -1, 1).onChange((v) => {
-    animator.speed = v;
-  });
-};
