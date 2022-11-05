6a47c62ac903d3bb02a35f8527b718a6972c6044
GuoLei1990 <gl3336563@163.com>
2022-11-05 23:31:41 +0800

    feat: update shadow sample

--- playground/shadow-basic.ts
+++ playground/shadow-basic.ts
@@ -3,49 +3,50 @@
  * @category Light
  */
 
-import {FreeControl} from "@oasis-engine-toolkit/controls";
+import { FreeControl } from "@oasis-engine-toolkit/controls";
 import {
-  Camera,
-  GLTFResource,
-  WebGLEngine,
-  Vector3,
-  DirectLight,
-  Color,
-  MeshRenderer,
   AmbientLight,
-  AssetType
+  AssetType,
+  Camera,
+  DirectLight,
+  GLTFResource,
+  MeshRenderer,
+  Vector3,
+  WebGLEngine
 } from "oasis-engine";
 
 const engine = new WebGLEngine("canvas");
 engine.canvas.resizeByClientSize();
-const scene = engine.sceneManager.activeScene;
-scene.ambientLight.diffuseSolidColor.set(1, 1, 1, 1);
 
+const scene = engine.sceneManager.activeScene;
+
+// Create root entity
 const rootEntity = scene.createRootEntity();
 
+// Create camera entity and component
 const cameraEntity = rootEntity.createChild("camera");
-cameraEntity.transform.setPosition(3, 2, 0.5);
-cameraEntity.transform.lookAt(new Vector3(0, 2, 0.5));
-const camera = cameraEntity.addComponent(Camera);
-camera.enableFrustumCulling = false;
+cameraEntity.transform.setPosition(3, 2.5, 0);
+cameraEntity.transform.lookAt(new Vector3(0, 2, 0));
+cameraEntity.addComponent(Camera);
 cameraEntity.addComponent(FreeControl).floorMock = false;
 
-// Create an entity to add light component
+// Create light entity and component
 const lightEntity = rootEntity.createChild("light");
-lightEntity.transform.setPosition(3, 2, 0.5);
-lightEntity.transform.lookAt(new Vector3(0, 2, 0.5));
+lightEntity.transform.setPosition(0.5, 1, 0);
+lightEntity.transform.lookAt(new Vector3(0, 0, 0));
 const directLight = lightEntity.addComponent(DirectLight);
-directLight.color = new Color(1.0, 1.0, 1.0);
 directLight.enableShadow = true;
-directLight.shadowBias = 5;
+directLight.shadowDistance = 20;
 
 engine.resourceManager
-  .load<GLTFResource>("https://gw.alipayobjects.com/os/bmw-prod/ca50859b-d736-4a3e-9fc3-241b0bd2afef.gltf")
+  .load<GLTFResource>(
+    "https://gw.alipayobjects.com/os/bmw-prod/ca50859b-d736-4a3e-9fc3-241b0bd2afef.gltf"
+  )
   .then((gltf) => {
-    rootEntity.addChild(gltf.defaultSceneRoot);
+    const root = gltf.defaultSceneRoot;
+    rootEntity.addChild(root);
 
-    const renderers: MeshRenderer[] = [];
-    gltf.defaultSceneRoot.getComponentsIncludeChildren(MeshRenderer, renderers);
+    const renderers = root.getComponentsIncludeChildren(MeshRenderer, []);
     for (let i = 0; i < renderers.length; i++) {
       const renderer = renderers[i];
       renderer.receiveShadows = true;
@@ -55,10 +56,10 @@ engine.resourceManager
     engine.resourceManager
       .load<AmbientLight>({
         type: AssetType.Env,
-        url: "https://gw.alipayobjects.com/os/bmw-prod/09904c03-0d23-4834-aa73-64e11e2287b0.bin"
+        url: "https://gw.alipayobjects.com/os/bmw-prod/09904c03-0d23-4834-aa73-64e11e2287b0.bin",
       })
       .then((ambientLight) => {
         scene.ambientLight = ambientLight;
         engine.run();
-      })
+      });
   });
