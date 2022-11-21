c603f02d1e534051a5ee1541da2523277c7df4e3
GuoLei1990 <gl3336563@163.com>
2022-11-21 15:33:18 +0800

    fix: playground link bug

--- playground/.dev/vite.config.js
+++ playground/.dev/vite.config.js
@@ -58,6 +58,11 @@ module.exports = {
     host: "0.0.0.0",
     port: 3000
   },
+  resolve:{
+    dedupe:[
+      "oasis-engine"
+    ]
+  },
   optimizeDeps: {
     exclude: [
       "oasis-engine",
