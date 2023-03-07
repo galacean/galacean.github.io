d801122a5ad0226c8729fc9779bf45249f43b3bf
GuoLei1990 <gl3336563@163.com>
2023-03-07 11:43:43 +0800

    refactor: fix framebuffer pick sample

--- playground/framebuffer-picker.ts
+++ playground/framebuffer-picker.ts
@@ -2,9 +2,17 @@
  * @title Framebuffer Picker
  * @category Toolkit
  */
-import { Camera, GLTFResource, PBRMaterial, PointerButton, Script, Vector3, WebGLEngine } from "oasis-engine";
 import { OrbitControl } from "@oasis-engine-toolkit/controls";
 import { FramebufferPicker } from "@oasis-engine-toolkit/framebuffer-picker";
+import {
+  Camera,
+  GLTFResource,
+  PBRMaterial,
+  PointerButton,
+  Script,
+  Vector3,
+  WebGLEngine
+} from "oasis-engine";
 
 class ClickScript extends Script {
   material: PBRMaterial;
@@ -15,13 +23,15 @@ class ClickScript extends Script {
     if (pointers && inputManager.isPointerDown(PointerButton.Primary)) {
       if (pointers.length > 0) {
         const pointerPosition = pointers[0].position;
-        framebufferPicker.pick(pointerPosition.x, pointerPosition.y).then((renderElement) => {
-          if (renderElement) {
-            this.material.baseColor.set(1, 0, 0, 1);
-          } else {
-            this.material.baseColor.set(1, 1, 1, 1);
-          }
-        });
+        const renderer = framebufferPicker.pick(
+          pointerPosition.x,
+          pointerPosition.y
+        );
+        if (renderer) {
+          this.material.baseColor.set(1, 0, 0, 1);
+        } else {
+          this.material.baseColor.set(1, 1, 1, 1);
+        }
       }
     }
   }
@@ -39,11 +49,12 @@ const control = cameraEntity.addComponent(OrbitControl);
 cameraEntity.transform.position = new Vector3(10, 10, 30);
 control.target.set(0, 3, 0);
 
-const framebufferPicker = rootNode.addComponent(FramebufferPicker);
-framebufferPicker.camera = camera;
+const framebufferPicker = cameraEntity.addComponent(FramebufferPicker);
 
 engine.resourceManager
-  .load<GLTFResource>("https://gw.alipayobjects.com/os/bmw-prod/f40ef8dd-4c94-41d4-8fac-c1d2301b6e47.glb")
+  .load<GLTFResource>(
+    "https://gw.alipayobjects.com/os/bmw-prod/f40ef8dd-4c94-41d4-8fac-c1d2301b6e47.glb"
+  )
   .then((gltf) => {
     const { defaultSceneRoot, materials } = gltf;
     const material = materials[0] as PBRMaterial;
