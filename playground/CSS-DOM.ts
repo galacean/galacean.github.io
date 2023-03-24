5207743d1d1b35a8fc81b3bd87ab69b9734048cf
GuoLei1990 <gl3336563@163.com>
2023-03-24 23:27:38 +0800

    refactor: opt code

--- playground/CSS-DOM.ts
+++ playground/CSS-DOM.ts
@@ -2,45 +2,62 @@
  * @title CSS DOM
  * @category 2D
  */
-import { AssetType, Camera, Entity, GLTFResource, Logger, Script, Vector3, WebGLEngine, WebGLMode } from "oasis-engine";
+import {
+  AssetType,
+  Camera,
+  Entity,
+  GLTFResource,
+  Logger,
+  Script,
+  Vector3,
+  WebGLEngine,
+  WebGLMode
+} from "oasis-engine";
 import { OrbitControl } from "oasis-engine-toolkit";
 Logger.enable();
 
-//-- create engine object
-const engine = new WebGLEngine("canvas", { webGLMode: WebGLMode.Auto });
-engine.canvas.resizeByClientSize();
+//Create engine object
+const engine = WebGLEngine.create({
+  canvas: "canvas",
+  graphicDeviceOptions: { webGLMode: WebGLMode.Auto },
+}).then((engine) => {
+  engine.canvas.resizeByClientSize();
 
-const scene = engine.sceneManager.activeScene;
-const rootEntity = scene.createRootEntity();
+  const scene = engine.sceneManager.activeScene;
+  const rootEntity = scene.createRootEntity();
 
-//Create camera
-const cameraNode = rootEntity.createChild("camera_node");
-cameraNode.transform.setPosition(0, 1.5, 5);
-const camera = cameraNode.addComponent(Camera);
-cameraNode.addComponent(OrbitControl);
+  //Create camera
+  const cameraNode = rootEntity.createChild("camera_node");
+  cameraNode.transform.setPosition(0, 1.5, 5);
+  const camera = cameraNode.addComponent(Camera);
+  cameraNode.addComponent(OrbitControl);
 
-engine.run();
+  engine.run();
 
-// load gltf
-engine.resourceManager
-  .load<GLTFResource>({
-    type: AssetType.Prefab,
-    url: "https://gw.alipayobjects.com/os/bmw-prod/8d36415b-5905-461f-9336-68a23d41518e.gltf"
-  })
-  .then((gltf) => {
-    console.log(gltf);
+  // load gltf
+  engine.resourceManager
+    .load<GLTFResource>({
+      type: AssetType.GLTF,
+      url: "https://gw.alipayobjects.com/os/bmw-prod/8d36415b-5905-461f-9336-68a23d41518e.gltf",
+    })
+    .then((gltf) => {
+      console.log(gltf);
 
-    const { defaultSceneRoot } = gltf;
-    rootEntity.addChild(defaultSceneRoot);
+      const { defaultSceneRoot } = gltf;
+      rootEntity.addChild(defaultSceneRoot);
 
-    // important !
-    defaultSceneRoot.addComponent(Renderer2DScript);
-  });
+      // important !
+      defaultSceneRoot.addComponent(Renderer2DScript);
+    });
+});
 
 // test dom
 const dom = document.createElement("div");
 dom.innerHTML = "Hello world!!!";
-dom.setAttribute("style", "padding:10px;position:absolute;top:0;left:0;background:white;border-radius:5px");
+dom.setAttribute(
+  "style",
+  "padding:10px;position:absolute;top:0;left:0;background:white;border-radius:5px"
+);
 document.body.appendChild(dom);
 
 class Renderer2DScript extends Script {
@@ -49,8 +66,10 @@ class Renderer2DScript extends Script {
   heightRatio: number;
   constructor(entity: Entity) {
     super(entity);
-    this.widthRatio = engine.canvas.width / engine.canvas._webCanvas.clientWidth;
-    this.heightRatio = engine.canvas.height / engine.canvas._webCanvas.clientHeight;
+    this.widthRatio =
+      engine.canvas.width / engine.canvas._webCanvas.clientWidth;
+    this.heightRatio =
+      engine.canvas.height / engine.canvas._webCanvas.clientHeight;
   }
 
   onUpdate() {
