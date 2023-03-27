a0266e33dddb74c6ba646d584b03206682ee77db
GuoLei1990 <gl3336563@163.com>
2023-03-27 21:36:29 +0800

    refactor: opt code

--- playground/physics-debug-draw.ts
+++ playground/physics-debug-draw.ts
@@ -6,125 +6,151 @@
 import { WireframeManager } from "@oasis-engine-toolkit/auxiliary-lines";
 import { OrbitControl } from "@oasis-engine-toolkit/controls";
 import {
-  AmbientLight, AssetType, BlinnPhongMaterial, BoxColliderShape, Camera, ColliderShape, DynamicCollider, MeshRenderer, PBRMaterial, PointLight,
-  PrimitiveMesh, Script, SphereColliderShape, StaticCollider, Vector3, WebGLEngine
+  AmbientLight,
+  AssetType,
+  BlinnPhongMaterial,
+  BoxColliderShape,
+  Camera,
+  ColliderShape,
+  DynamicCollider,
+  MeshRenderer,
+  PBRMaterial,
+  PointLight,
+  PrimitiveMesh,
+  Script,
+  SphereColliderShape,
+  StaticCollider,
+  Vector3,
+  WebGLEngine
 } from "oasis-engine";
 
-import {
-  PhysXPhysics
-} from "@oasis-engine/physics-physx";
+import { PhysXPhysics } from "@oasis-engine/physics-physx";
 
+WebGLEngine.create({ canvas: "canvas", physics: new PhysXPhysics() }).then(
+  (engine) => {
+    engine.canvas.resizeByClientSize();
+    const scene = engine.sceneManager.activeScene;
+    const rootEntity = scene.createRootEntity("root");
 
-WebGLEngine.create({ canvas: "canvas", physics: new PhysXPhysics() }).then((engine) => {
+    scene.ambientLight.diffuseSolidColor.set(1, 1, 1, 1);
+    scene.ambientLight.diffuseIntensity = 1.2;
 
-  engine.canvas.resizeByClientSize();
-  const scene = engine.sceneManager.activeScene;
-  const rootEntity = scene.createRootEntity("root");
+    // init camera
+    const cameraEntity = rootEntity.createChild("camera");
+    cameraEntity.addComponent(Camera);
+    cameraEntity.transform.setPosition(10, 10, 10);
+    cameraEntity.addComponent(OrbitControl);
 
-  scene.ambientLight.diffuseSolidColor.set(1, 1, 1, 1);
-  scene.ambientLight.diffuseIntensity = 1.2;
+    // init point light
+    const light = rootEntity.createChild("light");
+    light.transform.setPosition(0, 3, 0);
+    light.addComponent(PointLight);
 
-  // init camera
-  const cameraEntity = rootEntity.createChild("camera");
-  cameraEntity.addComponent(Camera);
-  cameraEntity.transform.setPosition(10, 10, 10);
-  cameraEntity.addComponent(OrbitControl);
+    // create box test entity
+    const cubeSize = 2.0;
+    const boxEntity = rootEntity.createChild("BoxEntity");
 
-  // init point light
-  const light = rootEntity.createChild("light");
-  light.transform.setPosition(0, 3, 0);
-  light.addComponent(PointLight);
+    const boxMtl = new PBRMaterial(engine);
+    const boxRenderer = boxEntity.addComponent(MeshRenderer);
+    boxMtl.baseColor.set(0.6, 0.3, 0.3, 1.0);
+    boxMtl.roughness = 0.5;
+    boxMtl.metallic = 0.0;
+    boxRenderer.mesh = PrimitiveMesh.createCuboid(
+      engine,
+      cubeSize,
+      cubeSize,
+      cubeSize
+    );
+    boxRenderer.setMaterial(boxMtl);
 
-  // create box test entity
-  const cubeSize = 2.0;
-  const boxEntity = rootEntity.createChild("BoxEntity");
+    const physicsBox = new BoxColliderShape();
+    physicsBox.size = new Vector3(cubeSize, cubeSize, cubeSize);
+    physicsBox.material.staticFriction = 0.1;
+    physicsBox.material.dynamicFriction = 0.2;
+    physicsBox.material.bounciness = 1;
+    physicsBox.isTrigger = true;
 
-  const boxMtl = new PBRMaterial(engine);
-  const boxRenderer = boxEntity.addComponent(MeshRenderer);
-  boxMtl.baseColor.set(0.6, 0.3, 0.3, 1.0);
-  boxMtl.roughness = 0.5;
-  boxMtl.metallic = 0.0;
-  boxRenderer.mesh = PrimitiveMesh.createCuboid(engine, cubeSize, cubeSize, cubeSize);
-  boxRenderer.setMaterial(boxMtl);
+    const boxCollider = boxEntity.addComponent(StaticCollider);
+    boxCollider.addShape(physicsBox);
 
-  const physicsBox = new BoxColliderShape();
-  physicsBox.size = new Vector3(cubeSize, cubeSize, cubeSize);
-  physicsBox.material.staticFriction = 0.1;
-  physicsBox.material.dynamicFriction = 0.2;
-  physicsBox.material.bounciness = 1;
-  physicsBox.isTrigger = true;
+    // create sphere test entity
+    const radius = 1.25;
+    const sphereEntity = rootEntity.createChild("SphereEntity");
+    sphereEntity.transform.setPosition(-2, 0, 0);
 
-  const boxCollider = boxEntity.addComponent(StaticCollider);
-  boxCollider.addShape(physicsBox);
+    const sphereMtl = new PBRMaterial(engine);
+    const sphereRenderer = sphereEntity.addComponent(MeshRenderer);
+    sphereMtl.baseColor.set(Math.random(), Math.random(), Math.random(), 1.0);
+    sphereMtl.metallic = 0.0;
+    sphereMtl.roughness = 0.5;
+    sphereRenderer.mesh = PrimitiveMesh.createSphere(engine, radius);
+    sphereRenderer.setMaterial(sphereMtl);
 
-  // create sphere test entity
-  const radius = 1.25;
-  const sphereEntity = rootEntity.createChild("SphereEntity");
-  sphereEntity.transform.setPosition(-2, 0, 0);
+    const physicsSphere = new SphereColliderShape();
+    physicsSphere.radius = radius;
+    physicsSphere.material.staticFriction = 0.1;
+    physicsSphere.material.dynamicFriction = 0.2;
+    physicsSphere.material.bounciness = 1;
 
-  const sphereMtl = new PBRMaterial(engine);
-  const sphereRenderer = sphereEntity.addComponent(MeshRenderer);
-  sphereMtl.baseColor.set(Math.random(), Math.random(), Math.random(), 1.0);
-  sphereMtl.metallic = 0.0;
-  sphereMtl.roughness = 0.5;
-  sphereRenderer.mesh = PrimitiveMesh.createSphere(engine, radius);
-  sphereRenderer.setMaterial(sphereMtl);
+    const sphereCollider = sphereEntity.addComponent(DynamicCollider);
+    sphereCollider.isKinematic = true;
+    sphereCollider.addShape(physicsSphere);
 
-  const physicsSphere = new SphereColliderShape();
-  physicsSphere.radius = radius;
-  physicsSphere.material.staticFriction = 0.1;
-  physicsSphere.material.dynamicFriction = 0.2;
-  physicsSphere.material.bounciness = 1;
+    const wireframe = rootEntity.addComponent(WireframeManager); // debug draw
+    wireframe.addEntityWireframe(sphereEntity);
+    wireframe.addEntityWireframe(boxEntity);
 
-  const sphereCollider = sphereEntity.addComponent(DynamicCollider);
-  sphereCollider.isKinematic = true;
-  sphereCollider.addShape(physicsSphere);
+    class MoveScript extends Script {
+      pos: number = -5;
+      vel: number = 0.05;
+      velSign: number = -1;
 
-  const wireframe = rootEntity.addComponent(WireframeManager); // debug draw
-  wireframe.addEntityWireframe(sphereEntity);
-  wireframe.addEntityWireframe(boxEntity);
-
-  class MoveScript extends Script {
-    pos: number = -5;
-    vel: number = 0.05;
-    velSign: number = -1;
-
-    onPhysicsUpdate() {
-      if (this.pos >= 5) {
-        this.velSign = -1;
+      onPhysicsUpdate() {
+        if (this.pos >= 5) {
+          this.velSign = -1;
+        }
+        if (this.pos <= -5) {
+          this.velSign = 1;
+        }
+        this.pos += this.vel * this.velSign;
+        this.entity.transform.worldPosition.set(this.pos, 0, 0);
       }
-      if (this.pos <= -5) {
-        this.velSign = 1;
+    }
+
+    // Collision Detection
+    class CollisionScript extends Script {
+      onTriggerExit(other: ColliderShape) {
+        (<BlinnPhongMaterial>sphereRenderer.getMaterial()).baseColor.set(
+          Math.random(),
+          Math.random(),
+          Math.random(),
+          1.0
+        );
       }
-      this.pos += this.vel * this.velSign;
-      this.entity.transform.worldPosition.set(this.pos, 0, 0);
+
+      onTriggerEnter(other: ColliderShape) {
+        (<BlinnPhongMaterial>sphereRenderer.getMaterial()).baseColor.set(
+          Math.random(),
+          Math.random(),
+          Math.random(),
+          1.0
+        );
+      }
+
+      onTriggerStay(other: ColliderShape) {}
     }
+
+    sphereEntity.addComponent(CollisionScript);
+    sphereEntity.addComponent(MoveScript);
+
+    engine.resourceManager
+      .load<AmbientLight>({
+        type: AssetType.Env,
+        url: "https://gw.alipayobjects.com/os/bmw-prod/89c54544-1184-45a1-b0f5-c0b17e5c3e68.bin",
+      })
+      .then((ambientLight) => {
+        scene.ambientLight = ambientLight;
+        engine.run();
+      });
   }
-
-  // Collision Detection
-  class CollisionScript extends Script {
-    onTriggerExit(other: ColliderShape) {
-      (<BlinnPhongMaterial>sphereRenderer.getMaterial()).baseColor.set(Math.random(), Math.random(), Math.random(), 1.0);
-    }
-
-    onTriggerEnter(other: ColliderShape) {
-      (<BlinnPhongMaterial>sphereRenderer.getMaterial()).baseColor.set(Math.random(), Math.random(), Math.random(), 1.0);
-    }
-
-    onTriggerStay(other: ColliderShape) {
-    }
-  }
-
-  sphereEntity.addComponent(CollisionScript);
-  sphereEntity.addComponent(MoveScript);
-
-  engine.resourceManager
-    .load<AmbientLight>({
-      type: AssetType.Env,
-      url: "https://gw.alipayobjects.com/os/bmw-prod/89c54544-1184-45a1-b0f5-c0b17e5c3e68.bin"
-    })
-    .then((ambientLight) => {
-      scene.ambientLight = ambientLight;
-      engine.run();
-    });
-});
+);
