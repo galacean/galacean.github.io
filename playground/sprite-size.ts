88ac07f7e04fe8095b90a30b5907c40fa3d2702f
cptbtptpbcptdtptp <njktsmshh@163.com>
2023-04-23 12:10:32 +0800

    feat: update sprite doc

---
+++ playground/sprite-size.ts
@@ -0,0 +1,76 @@
+/**
+ * @title Sprite Size
+ * @category 2D
+ */
+import * as dat from "dat.gui";
+import {
+  AssetType,
+  Camera,
+  Entity,
+  Sprite,
+  SpriteRenderer,
+  Texture2D,
+  WebGLEngine,
+} from "@galacean/engine";
+
+// Create engine object.
+const engine = new WebGLEngine("canvas");
+engine.canvas.resizeByClientSize();
+
+// Create root entity.
+const rootEntity = engine.sceneManager.activeScene.createRootEntity();
+
+// Create camera.
+const cameraEntity = rootEntity.createChild("Camera");
+cameraEntity.transform.setPosition(0, 0, 10);
+const camera = cameraEntity.addComponent(Camera);
+camera.isOrthographic = true;
+camera.orthographicSize = 5;
+
+engine.resourceManager
+  .load<Texture2D>({
+    url: "https://gw.alipayobjects.com/mdn/rms_7c464e/afts/img/A*d3N9RYpcKncAAAAAAAAAAAAAARQnAQ",
+    type: AssetType.Texture2D,
+  })
+  .then((texture) => {
+    // Create origin sprite entity.
+    const entity = rootEntity.createChild("sprite");
+    entity.addComponent(SpriteRenderer).sprite = new Sprite(engine, texture);
+    addDataGUI(entity);
+  });
+engine.run();
+
+/**
+ * Add data GUI.
+ */
+function addDataGUI(entity: Entity) {
+  const spriteRenderer = entity.getComponent(SpriteRenderer);
+  const sprite = spriteRenderer.sprite;
+  const gui = new dat.GUI();
+  const defaultWidth = sprite.width;
+  const defaultHeight = sprite.height;
+  const guiData = {
+    width: defaultWidth,
+    height: defaultHeight,
+    reset: () => {
+      spriteRenderer.width = guiData.width = defaultWidth;
+      spriteRenderer.height = guiData.height = defaultHeight;
+    },
+  };
+
+  gui
+    .add(guiData, "width", 0, defaultWidth * 5, defaultWidth / 10)
+    .onChange((value: number) => {
+      spriteRenderer.width = value;
+    })
+    .listen();
+  gui
+    .add(guiData, "height", 0, defaultHeight * 5, defaultHeight / 10)
+    .onChange((value: number) => {
+      spriteRenderer.height = value;
+    })
+    .listen();
+  gui.add(guiData, "reset").name("重置");
+
+  return guiData;
+}
