da8c08fc8c345c4ac053427cca6dd8c4d0ed5a7e
GuoLei1990 <gl3336563@163.com>
2023-07-20 14:52:33 +0800

    refactor: opt code

--- playground/shader-lab.ts
+++ playground/shader-lab.ts
@@ -17,8 +17,8 @@ import {
   Texture2D,
   Vector3,
   WebGLEngine,
-} from '@galacean/engine';
-import { ShaderLab } from '@galacean/engine-shaderlab';
+} from "@galacean/engine";
+import { ShaderLab } from "@galacean/engine-shaderlab";
 
 // Create ShaderLab
 const shaderLab = new ShaderLab();
@@ -27,74 +27,75 @@ main();
 
 async function main() {
   // Create engine
-  const engine = await WebGLEngine.create({ canvas: 'canvas', shaderLab });
+  const engine = await WebGLEngine.create({ canvas: "canvas", shaderLab });
+  engine.canvas.resizeByClientSize();
+
   // Custom shader
-  const shaderSource = `Shader Water {
-  SubShader {
+  const shaderSource = `
+  Shader Water {
+    SubShader {
+      Pass Water {
+        struct Attributes {
+          vec4 POSITION;
+          vec2 TEXCOORD_0; 
+        }
 
-    Pass Water {
+        struct Varyings {
+          vec2 uv;
+          vec3 position;
+        }
 
-      struct a2v {
-       vec4 POSITION;
-       vec2 TEXCOORD_0; 
-      }
+        mat4 renderer_MVPMat;
+        mat4 renderer_MVMat;
 
-      struct v2f {
-       vec2 v_uv;
-       vec3 v_position;
-      }
+        sampler2D material_BaseTexture;
+        vec4 u_color;
+        vec4 u_fogColor;
+        float u_fogDensity;
+        
+        VertexShader = vert;
+        FragmentShader = frag;
 
-      mat4 renderer_MVPMat;
-      mat4 renderer_MVMat;
+       
 
-      sampler2D material_BaseTexture;
-      vec4 u_color;
-      vec4 u_fogColor;
-      float u_fogDensity;
-      
-      VertexShader = vert;
-      FragmentShader = frag;
+        Varyings vert(Attributes input) {
+          Varyings v;
 
-      vec4 linearToGamma(vec4 linearIn) {
+          v.uv = input.TEXCOORD_0;
+          vec4 tmp = renderer_MVMat * POSITION;
+          v.position = tmp.xyz;
+          gl_Position = renderer_MVPMat * input.POSITION;
+          return v;
+        }
+
+        void frag(Varyings v) {
+          vec4 color = texture2D(material_BaseTexture, v.uv) * u_color;
+          float fogDistance = length(v.position);
+          float fogAmount = 1.0 - exp2(-u_fogDensity * u_fogDensity * fogDistance * fogDistance * 1.442695);
+          fogAmount = clamp(fogAmount, 0.0, 1.0);
+          gl_FragColor = mix(color, u_fogColor, fogAmount); 
+    
+          #ifndef ENGINE_IS_COLORSPACE_GAMMA
+            gl_FragColor = linearToGamma(gl_FragColor);
+          #endif
+        }
+
+        vec4 linearToGamma(vec4 linearIn) {
           return vec4(pow(linearIn.rgb, vec3(1.0 / 2.2)), linearIn.a);
-    }
-
-      v2f vert(a2v v) {
-        v2f o;
-
-        o.v_uv = v.TEXCOORD_0;
-        vec4 tmp = renderer_MVMat * POSITION;
-        o.v_position = tmp.xyz;
-        gl_Position = renderer_MVPMat * v.POSITION;
-        return o;
-      }
-
-      void frag(v2f i) {
-        vec4 color = texture2D(material_BaseTexture, i.v_uv) * u_color;
-        float fogDistance = length(i.v_position);
-        float fogAmount = 1.0 - exp2(-u_fogDensity * u_fogDensity * fogDistance * fogDistance * 1.442695);
-        fogAmount = clamp(fogAmount, 0.0, 1.0);
-        gl_FragColor = mix(color, u_fogColor, fogAmount); 
-  
-        #ifndef ENGINE_IS_COLORSPACE_GAMMA
-          gl_FragColor = linearToGamma(gl_FragColor);
-        #endif
+        }
       }
     }
   }
-}
 `;
 
   // Init shader
   Shader.create(shaderSource);
 
-  engine.canvas.resizeByClientSize();
-
   // Create root entity
   const rootEntity = engine.sceneManager.activeScene.createRootEntity();
 
   // Create camera
-  const cameraEntity = rootEntity.createChild('Camera');
+  const cameraEntity = rootEntity.createChild("Camera");
   cameraEntity.transform.setPosition(0, 10, 10);
   cameraEntity.transform.lookAt(new Vector3(0, 8, 0));
   const camera = cameraEntity.addComponent(Camera);
@@ -102,6 +103,7 @@ async function main() {
   camera.fieldOfView = 55;
 
   createPlane(engine, rootEntity);
+  
   engine.run();
 }
 
@@ -111,15 +113,14 @@ async function main() {
 function createPlane(engine: Engine, entity: Entity): void {
   engine.resourceManager
     .load<Texture2D>({
-      url: 'https://gw.alipayobjects.com/mdn/rms_2e421e/afts/img/A*fRtNTKrsq3YAAAAAAAAAAAAAARQnAQ',
+      url: "https://gw.alipayobjects.com/mdn/rms_2e421e/afts/img/A*fRtNTKrsq3YAAAAAAAAAAAAAARQnAQ",
       type: AssetType.Texture2D,
     })
     .then((texture) => {
-      const planeEntity = entity.createChild('plane');
+      const planeEntity = entity.createChild("plane");
       const meshRenderer = planeEntity.addComponent(MeshRenderer);
-      const material = new Material(engine, Shader.find('Water'));
+      const material = new Material(engine, Shader.find("Water"));
 
-      // planeEntity.transform.setRotation(-90, 0, 0);
       meshRenderer.mesh = PrimitiveMesh.createPlane(
         engine,
         1245,
@@ -133,11 +134,11 @@ function createPlane(engine: Engine, entity: Entity): void {
       planeEntity.addComponent(PlaneAnimation);
 
       const { shaderData } = material;
-      shaderData.setTexture('material_BaseTexture', texture);
-      shaderData.setColor('u_fogColor', new Color(0.25, 0.25, 0.25, 1));
-      shaderData.setFloat('u_fogDensity', 0.004);
+      shaderData.setTexture("material_BaseTexture", texture);
+      shaderData.setColor("u_fogColor", new Color(0.25, 0.25, 0.25, 1));
+      shaderData.setFloat("u_fogDensity", 0.004);
       shaderData.setColor(
-        'u_color',
+        "u_color",
         new Color(86 / 255, 182 / 255, 194 / 255, 1)
       );
     });
@@ -159,7 +160,7 @@ class PlaneAnimation extends Script {
     const renderer = this.entity.getComponent(MeshRenderer);
     const mesh = <ModelMesh>renderer.mesh;
     const { vertexCount } = mesh;
-    const positions = mesh.getPositions();
+    const positions = mesh.getPositions()!;
     const initY = new Array<number>(vertexCount);
 
     for (var i = 0; i < vertexCount; i++) {
@@ -179,7 +180,7 @@ class PlaneAnimation extends Script {
   onUpdate(deltaTime: number): void {
     const mesh = this._planeMesh;
     let { _counter: counter, _initZ: initZ } = this;
-    const positions = mesh.getPositions();
+    const positions = mesh.getPositions()!;
     for (let i = 0, n = positions.length; i < n; i++) {
       const position = positions[i];
       position.y =
