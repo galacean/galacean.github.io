4c13e23d58c8e6c7881dadb942d8318ec169cccf
GuoLei1990 <gl3336563@163.com>
2023-08-14 13:01:41 +0800

    refactor: opt code

--- playground/particle-renderer.ts
+++ playground/particle-renderer.ts
@@ -6,6 +6,7 @@ import {
   AssetType,
   Camera,
   Color,
+  ParticleMaterial,
   ParticleRenderer,
   Texture2D,
   Vector3,
@@ -21,40 +22,26 @@ WebGLEngine.create({ canvas: "canvas" }).then((engine) => {
 
   // Create camera
   const cameraEntity = rootEntity.createChild("camera_entity");
-  cameraEntity.transform.position = new Vector3(0, 0, 50);
+  cameraEntity.transform.position = new Vector3(0, 0, 10);
   cameraEntity.addComponent(Camera);
 
   engine.run();
 
   const particleEntity = rootEntity.createChild("particle");
-
-  let particles: ParticleRenderer =
-    particleEntity.addComponent(ParticleRenderer);
+  const particleRenderer = particleEntity.addComponent(ParticleRenderer);
+  const material = new ParticleMaterial(engine);
+  particleRenderer.setMaterial(material);
 
   engine.resourceManager
     .load<Texture2D>({
       url: "https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*kxloQYq2YDEAAAAAAAAAAAAAARQnAQ",
       type: AssetType.Texture2D,
     })
-    .then((resource) => {
-      particles.maxCount = 100;
-      particles.startTimeRandomness = 10;
-      particles.lifetime = 4;
-      particles.position = new Vector3(0, 20, 0);
-      particles.positionRandomness = new Vector3(100, 0, 0);
-      particles.velocity = new Vector3(0, -3, 0);
-      particles.velocityRandomness = new Vector3(1, 2, 0);
-      particles.accelerationRandomness = new Vector3(0, 1, 0);
-      particles.velocityRandomness = new Vector3(-1, -1, -1);
-      particles.rotateVelocity = 1;
-      particles.rotateVelocityRandomness = 1;
-      particles.size = 1;
-      particles.sizeRandomness = 0.8;
-      particles.color = new Color(0.5, 0.5, 0.5);
-      particles.colorRandomness = 1;
-      particles.isFadeIn = true;
-      particles.isFadeOut = true;
-      particles.texture = resource;
-      particles.start();
+    .then((texture) => {
+      const generator = particleRenderer.generator;
+      generator.shape.enabled = false;
+
+      material.baseTexture = texture;
+      particleRenderer.play();
     });
 });
