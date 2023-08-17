d9899590e7295ee7e7569336c2494b699fab799e
GuoLei1990 <gl3336563@163.com>
2023-08-18 02:23:20 +0800

    refactor: opt code

--- playground/particle-renderer.ts
+++ playground/particle-renderer.ts
@@ -4,19 +4,25 @@
  */
 import {
   AssetType,
+  BlendMode,
   Camera,
   Logger,
+  ParticleCurveMode,
   ParticleMaterial,
   ParticleRenderer,
   RenderFace,
   Texture2D,
+  Vector2,
   Vector3,
   WebGLEngine,
 } from "@galacean/engine";
-
+import { WebGLMode } from "oasis-engine";
 
 // Create engine
-WebGLEngine.create({ canvas: "canvas" }).then((engine) => {
+WebGLEngine.create({
+  canvas: "canvas",
+  graphicDeviceOptions: { webGLMode: WebGLMode.WebGL1 },
+}).then((engine) => {
   Logger.enable();
   engine.canvas.resizeByClientSize();
 
@@ -25,27 +31,55 @@ WebGLEngine.create({ canvas: "canvas" }).then((engine) => {
 
   // Create camera
   const cameraEntity = rootEntity.createChild("camera_entity");
-  cameraEntity.transform.position = new Vector3(0, 0, 10);
-  cameraEntity.addComponent(Camera);
+  cameraEntity.transform.position = new Vector3(0, 5, 10);
+  const camera = cameraEntity.addComponent(Camera);
+  camera.fieldOfView = 60;
 
   engine.run();
 
   const particleEntity = rootEntity.createChild("particle");
+  particleEntity.transform.scale.set(1.268892, 1.268892, 1.268892);
   particleEntity.transform.rotate(90, 0, 0);
 
   const particleRenderer = particleEntity.addComponent(ParticleRenderer);
   const material = new ParticleMaterial(engine);
-  material.renderFace = RenderFace.Double;
+  material.blendMode = BlendMode.Additive;
   particleRenderer.setMaterial(material);
 
   engine.resourceManager
     .load<Texture2D>({
-      url: "https://gw.alipayobjects.com/mdn/rms_d27172/afts/img/A*kxloQYq2YDEAAAAAAAAAAAAAARQnAQ",
+      url: "https://mdn.alipayobjects.com/huamei_b4l2if/afts/img/A*yu-DSb0surwAAAAAAAAAAAAADil6AQ/original",
       type: AssetType.Texture2D,
     })
     .then((texture) => {
       const generator = particleRenderer.generator;
+
+      generator.main.startLifetime.constantMin = 0.2;
+      generator.main.startLifetime.constantMax = 0.8;
+      generator.main.startLifetime.mode = ParticleCurveMode.TwoConstants;
+
+
+      generator.main.startSpeed.constantMin = 0.4;
+      generator.main.startSpeed.constantMax = 1.6;
+      generator.main.startSpeed.mode = ParticleCurveMode.TwoConstants;
+
+      generator.main.startSize.constantMin = 0.6;
+      generator.main.startSize.constantMax = 0.9;
+      generator.main.startSize.mode = ParticleCurveMode.TwoConstants;
+
+      generator.main.startRotation.constantMin = 0;
+      generator.main.startRotation.constantMax = 360;
+      generator.main.startRotation.mode = ParticleCurveMode.TwoConstants;
+
+      generator.emission.rateOverTime.constant = 1;
+
       generator.shape.enabled = false;
+
+      generator.textureSheetAnimation.enabled = true;
+      generator.textureSheetAnimation.tiling = new Vector2(6, 6);
+
+      // generator.sizeOverLifetime.enabled = true;
+      // generator.colorOverLifetime.enabled = true;
 
       material.baseTexture = texture;
       particleRenderer.play();
