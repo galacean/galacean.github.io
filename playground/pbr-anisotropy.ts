1be51f98b1ac81194ca31f747f582bc319027896
zhuxudong <callzhuxudong@163.com>
2023-12-21 20:19:05 +0800

    chore: change demo

--- playground/pbr-anisotropy.ts
+++ playground/pbr-anisotropy.ts
@@ -5,14 +5,12 @@
 import {
   AmbientLight,
   AssetType,
-  BackgroundMode,
   Camera,
   DirectLight,
-  GLTFResource,
+  MeshRenderer,
   PBRMaterial,
   PrimitiveMesh,
   Script,
-  SkyBoxMaterial,
   Vector3,
   WebGLEngine
 } from "@galacean/engine";
@@ -26,74 +24,76 @@ WebGLEngine.create({ canvas: "canvas" }).then((engine) => {
   const scene = engine.sceneManager.activeScene;
   const rootEntity = scene.createRootEntity();
   const cameraEntity = rootEntity.createChild("camera");
-  const target = new Vector3(0, 1.4, 0);
   cameraEntity.addComponent(Camera);
-  cameraEntity.transform.setPosition(0, target.y, 0.8);
-  cameraEntity.transform.lookAt(target);
-  const control = cameraEntity.addComponent(OrbitControl);
-  control.target = target;
-  control.minDistance = 0.5;
-  control.maxDistance = 3;
+  cameraEntity.transform.setPosition(0, 2, 2.5);
+  cameraEntity.transform.lookAt(new Vector3(0, 0, 0));
+  cameraEntity.addComponent(OrbitControl);
 
-  // Create sky
-  const sky = scene.background.sky;
-  const skyMaterial = new SkyBoxMaterial(engine);
-  scene.background.mode = BackgroundMode.Sky;
-
-  sky.material = skyMaterial;
-  sky.mesh = PrimitiveMesh.createCuboid(engine, 1, 1, 1);
-
-  const directLightNodeFront = rootEntity.createChild("dir_light_front");
-  const directLightNodeBack = rootEntity.createChild("dir_light_back");
-  directLightNodeFront.addComponent(DirectLight);
-  directLightNodeBack.addComponent(DirectLight);
-  directLightNodeBack.transform.setRotation(0, 180, 0);
+  const lightEntity = rootEntity.createChild("light");
+  const light = lightEntity.addComponent(DirectLight);
+  lightEntity.transform.setRotation(-50, 180, 0);
+  light.color.set(0.1, 0, 0);
 
   class Rotate extends Script {
     onUpdate(deltaTime: number): void {
-      this.entity.transform.rotate(100 * deltaTime, 0, 0);
+      this.entity.transform.rotate(0, 50 * deltaTime, 0);
     }
   }
-
-  const rotateComponent = directLightNodeFront.addComponent(Rotate);
 
   engine.resourceManager
     .load<AmbientLight>({
       type: AssetType.Env,
-      url: "https://gw.alipayobjects.com/os/bmw-prod/89c54544-1184-45a1-b0f5-c0b17e5c3e68.bin"
+      url: "https://mdn.alipayobjects.com/oasis_be/afts/file/A*4zvFQaMWvOsAAAAAAAAAAAAADkp5AQ/ambient.bin"
     })
     .then((ambientLight) => {
       scene.ambientLight = ambientLight;
-      skyMaterial.texture = ambientLight.specularTexture;
-      skyMaterial.textureDecodeRGBM = true;
       engine.run();
     });
 
-  engine.resourceManager
-    .load<GLTFResource>("https://mdn.alipayobjects.com/oasis_be/afts/file/A*qEVES6mkMjkAAAAAAAAAAAAADkp5AQ/hair.glb")
-    .then((gltf) => {
-      const hairMat = gltf.materials![0] as PBRMaterial;
-      rootEntity.addChild(gltf.defaultSceneRoot);
+  const entity = rootEntity.createChild("entity");
+  const renderer = entity.addComponent(MeshRenderer);
+  const material = new PBRMaterial(engine);
+  material.metallic = 1;
+  material.roughness = 0.3;
+  material.anisotropy = 1;
+  material.anisotropyDirection.set(1, 1);
+  renderer.mesh = PrimitiveMesh.createCylinder(engine, 1, 1, 0, 64);
+  renderer.setMaterial(material);
 
-      hairMat.metallic = 0;
-      hairMat.anisotropy = 0.8;
+  engine.resourceManager
+    .load({
+      type: AssetType.Texture2D,
+      url: "https://mdn.alipayobjects.com/huamei_dmxymu/afts/img/A*dQ44Q6E5TzUAAAAAAAAAAAAADuuHAQ/original"
+    })
+    .then((texture) => {
+      material.anisotropyTexture = texture;
+      const rotateComponent = entity.addComponent(Rotate);
 
       const debugInfo = {
         aniso_x: 1,
-        aniso_y: 0,
-        rotate: true
+        aniso_y: 1,
+        rotate: true,
+        texture: true
       };
 
-      gui.add(hairMat, "anisotropy", 0, 1, 0.01);
+      gui.add(material, "anisotropy", 0, 1, 0.01);
       gui.add(debugInfo, "aniso_x", 0, 1, 0.01).onChange((v) => {
-        hairMat.anisotropyDirection.x = v;
+        material.anisotropyDirection.x = v;
       });
       gui.add(debugInfo, "aniso_y", 0, 1, 0.01).onChange((v) => {
-        hairMat.anisotropyDirection.y = v;
+        material.anisotropyDirection.y = v;
       });
 
       gui.add(debugInfo, "rotate").onChange((v) => {
         rotateComponent.enabled = v;
+      });
+
+      gui.add(debugInfo, "texture").onChange((v) => {
+        if (v) {
+          material.anisotropyTexture = texture;
+        } else {
+          material.anisotropyTexture = null;
+        }
       });
     });
 });
