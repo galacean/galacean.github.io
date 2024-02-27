494c383e5879a878efe9724fec0fb7fa70c7ab3a
zhuxudong <callzhuxudong@163.com>
2024-02-27 23:30:44 +0800

    chore: simple code

--- playground/shader-lab-multi-pass.ts
+++ playground/shader-lab-multi-pass.ts
@@ -34,15 +34,13 @@ Shader.create(
   "fur-shader",
   `
     attribute vec4 POSITION;
-    attribute vec2 TEXCOORD_0;
     attribute vec3 NORMAL;
+    attribute vec2 TEXCOORD_0;
 
     uniform mat4 renderer_MVPMat;
     uniform mat4 renderer_ModelMat;
     uniform mat4 renderer_NormalMat;
 
-    uniform vec3 u_gravity;
-    uniform float u_gravityIntensity;
     uniform float u_furLength;
     uniform float u_furOffset;
     uniform vec4 u_uvTilingOffset;
@@ -50,19 +48,12 @@ Shader.create(
 
     varying vec2 v_uv;
     varying vec2 v_uv2;
-    varying vec3 v_normal;
-    varying vec3 v_pos;
 
     void main(){
       vec4 position = POSITION;
-      vec3 direction = mix(NORMAL, u_gravity * u_gravityIntensity + NORMAL * (1.0 - u_gravityIntensity), u_furOffset);
-      position.xyz += direction * u_furLength * u_furOffset;
+      position.xyz += NORMAL * u_furLength * u_furOffset;
       gl_Position = renderer_MVPMat * position;
   
-
-      v_normal = normalize( mat3(renderer_NormalMat) * NORMAL );
-      vec4 temp_pos = renderer_ModelMat * position;
-      v_pos = temp_pos.xyz / temp_pos.w;
 
       vec2 uvOffset = u_uvTilingOffset.zw * u_furOffset;
       v_uv = TEXCOORD_0 + uvOffset * vec2(1.0, 1.0) / u_uvTilingOffset.xy;
@@ -79,13 +70,9 @@ Shader.create(
 
     varying vec2 v_uv;
     varying vec2 v_uv2;
-    varying vec3 v_normal;
-    varying vec3 v_pos;
     
 
     void main(){
-			  vec3 normalDirection = normalize(v_normal);
-			  vec3 viewDirection =  normalize(camera_Position - v_pos);
 			  vec4 baseColor = texture2D(u_mainTex, v_uv);
         float alpha2 = u_furOffset * u_furOffset;
 
@@ -113,11 +100,6 @@ WebGLEngine.create({ canvas: "canvas", shaderLab }).then((engine) => {
   cameraEntity.addComponent(Camera);
   cameraEntity.addComponent(OrbitControl);
 
-  // direct light
-  const directLightNode = rootEntity.createChild("dir_light");
-  directLightNode.addComponent(DirectLight);
-  directLightNode.transform.setRotation(30, 30, 0);
-
   engine.resourceManager
     .load([
       {
@@ -127,10 +109,6 @@ WebGLEngine.create({ canvas: "canvas", shaderLab }).then((engine) => {
       {
         type: AssetType.GLTF,
         url: "https://gw.alipayobjects.com/os/OasisHub/267000040/9994/%25E5%25BD%2592%25E6%25A1%25A3.gltf"
-      },
-      {
-        type: AssetType.Env,
-        url: "https://gw.alipayobjects.com/os/bmw-prod/f369110c-0e33-47eb-8296-756e9c80f254.bin"
       }
     ])
     .then((res) => {
@@ -138,7 +116,6 @@ WebGLEngine.create({ canvas: "canvas", shaderLab }).then((engine) => {
       const glTF = res[1] as GLTFResource;
       const oriMaterial = glTF.materials![0] as PBRMaterial;
 
-      scene.ambientLight = res[2] as AmbientLight;
       // const entity = glTF.defaultSceneRoot.clone();
       // rootEntity.addChild(entity);
       // const renderer = entity.getComponentsIncludeChildren(MeshRenderer, [])[0];
@@ -146,7 +123,7 @@ WebGLEngine.create({ canvas: "canvas", shaderLab }).then((engine) => {
       // create sphere
       const entity = rootEntity.createChild("sphere");
       const renderer = entity.addComponent(MeshRenderer);
-      renderer.mesh = PrimitiveMesh.createSphere(engine, 1, 32);
+      renderer.mesh = PrimitiveMesh.createSphere(engine, 0.5, 32);
 
       const material = new BaseMaterial(engine, Shader.find("fur-shader"));
       renderer.setMaterial(material);
@@ -158,15 +135,12 @@ WebGLEngine.create({ canvas: "canvas", shaderLab }).then((engine) => {
       shaderData.setTexture("u_mainTex", oriMaterial.baseTexture);
       shaderData.setTexture("u_layerTex", layerTexture);
 
-      shaderData.setFloat("u_furLength", 0.5);
-      shaderData.setVector3("u_gravity", new Vector3(0, -1, 0));
-      shaderData.setFloat("u_gravityIntensity", 0.25);
+      shaderData.setFloat("u_furLength", 0.25);
       shaderData.setVector4("u_uvTilingOffset", new Vector4(5, 5, 0.2, 0.2));
 
       const debugInfo = {
         layer: 20,
-        u_furLength: 0.5,
-        u_gravityIntensity: 0.25,
+        u_furLength: 0.25,
         uvScale: 5,
         uvOffset: 0.2
       };
@@ -174,11 +148,8 @@ WebGLEngine.create({ canvas: "canvas", shaderLab }).then((engine) => {
       gui.add(debugInfo, "layer", 0, 40, 1).onChange((v) => {
         script.layer = v;
       });
-      gui.add(debugInfo, "u_furLength", 0, 10, 0.01).onChange((v) => {
+      gui.add(debugInfo, "u_furLength", 0, 1, 0.01).onChange((v) => {
         shaderData.setFloat("u_furLength", v);
-      });
-      gui.add(debugInfo, "u_gravityIntensity", 0, 1, 0.01).onChange((v) => {
-        shaderData.setFloat("u_gravityIntensity", v);
       });
       gui.add(debugInfo, "uvScale", 0, 50, 1).onChange((v) => {
         const value = shaderData.getVector4("u_uvTilingOffset");
